983a3956093ade9fb817ff18f73641bdbba98f52
Soren Schwartz <s_schwartz@live.dk>
2021-10-31 21:28:27 +0100

    day 3 c++

--- year2019/c++/day3/day3.cpp
+++ year2019/c++/day3/day3.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <set>
 #include <sstream>
 #include <string>
 #include <tuple>
+#include <cmath>
 
 using namespace std;
 
@@ -42,21 +44,18 @@ map<int, vector<string>> readInput() {
 		wires[i] = input;
 	}
 
-	//for (auto const& x : wires)
-	//{
-	//	cout << x.first << ':' << x.second.size() << std::endl;
-	//}
-
 	return wires;
 }
 
-void addSteps(map<char, int> coordinates, char coord, int multiplier, int value, map<tuple<int, int>, int> places) {
+void addSteps(map<char, int> &coordinates, char coord, int multiplier, int value, map<tuple<int, int>, int> &places, int steps) {
 	int position_before = coordinates[coord];
 	int position_after = position_before + multiplier * value;
 
-	int steps = coordinates['s'] + 1;
+	auto loopCondition = [](int i, int multiplier, int position_after) {
+		return (multiplier > 0 ? i < position_after : i > position_after + multiplier);
+	};
 
-	for (int i = position_before + multiplier; i < position_after + multiplier; i += multiplier) {
+	for (int i = position_before + multiplier; loopCondition(i, multiplier, position_after); i += multiplier) {
 		tuple<int, int> place;
 		if (coord == 'x') {
 			place = make_tuple(i, coordinates['y']);
@@ -65,6 +64,7 @@ void addSteps(map<char, int> coordinates, char coord, int multiplier, int value,
 			place = make_tuple(coordinates['x'], i);
 		}
 		places[place] = steps;
+
 		steps++;
 	}
 
@@ -90,23 +90,37 @@ map<tuple<int, int>, int> getCoordinates(vector<string> wire) {
 		string key = direction.substr(0, 1);
 		int value = stoi(direction.substr(1, direction.size()));
 
-		if (key == "U") {
-			addSteps(coordinates, 'x', 1, value, places);
-		}
-		if (key == "D") {
-			addSteps(coordinates, 'x', -1, value, places);
-		}
-		if (key == "R") {
-			addSteps(coordinates, 'y', 1, value, places);
-		}
-		if (key == "L") {
-			addSteps(coordinates, 'y', -1, value, places);
-		}
+		int steps = coordinates['s'] + 1;
 
-		cout << key << " : " << value << endl;
+		if (key == "U") {
+			addSteps(coordinates, 'x', 1, value, places, steps);
+		}
+		else if (key == "D") {
+			addSteps(coordinates, 'x', -1, value, places, steps);
+		}
+		else if (key == "R") {
+			addSteps(coordinates, 'y', 1, value, places, steps);
+		}
+		else if (key == "L") {
+			addSteps(coordinates, 'y', -1, value, places, steps);
+		} else {
+			cout << "Error: " << key << endl;
+		}
 	}
 
 	return places;
+}
+
+template <class T, class U>
+set<T> getSetFromKeys(map<T,U> mapWithKeys){
+	
+	set<T> outputSet;
+	
+	for (pair<const T, U> keyValuePair : mapWithKeys) {
+		outputSet.insert(keyValuePair.first);
+	}
+
+	return outputSet;	
 }
 
 int main() {
@@ -115,6 +129,40 @@ int main() {
 	map<tuple<int, int>, int> wireOne = getCoordinates(wires[0]);
 	map<tuple<int, int>, int> wireSecond = getCoordinates(wires[1]);
 
+	set<tuple<int, int>> placesWireOne = getSetFromKeys(wireOne);
+	set<tuple<int, int>> placesWireSecond = getSetFromKeys(wireSecond);
+
+	set<tuple<int,int>> intersect;
+	set_intersection(placesWireOne.begin(), placesWireOne.end(),
+					placesWireSecond.begin(), placesWireSecond.end(),
+					std::inserter(intersect, intersect.begin()));
+	
+	vector<int> distances;
+	for (auto elem : intersect) {
+		int xCoordinate = get<0>(elem);
+		int yCoordinate = get<1>(elem);
+
+		if (xCoordinate != 0 || yCoordinate != 0) {
+			distances.push_back(abs(xCoordinate) + abs(yCoordinate));
+		}
+	}
+
+	int minimalDistance = *min_element(distances.begin(), distances.end());
+
+	cout << "Part 1: " << minimalDistance << endl;
+
+	vector<int> steps;
+	for(auto elem : intersect) {
+		if (elem != make_tuple(0,0)) {
+			// cout << wireOne[elem] + wireSecond[elem] << endl;
+			steps.push_back(wireOne[elem] + wireSecond[elem]);
+		}
+	}
+
+	int minimalSteps = *min_element(steps.begin(), steps.end());
+
+	cout << "Part 2: " << minimalSteps << endl;
+
 	/*for (auto &wire : wires) {
 		getCoordinates(wire.second);
 	}*/
@@ -122,4 +170,3 @@ int main() {
 	return 0;
 
 }
-
