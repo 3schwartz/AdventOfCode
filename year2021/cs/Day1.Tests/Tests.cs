c769b1195605f1e554c47ad0ab8a9dfb0bcd4314
Soren Schwartz <s_schwartz@live.dk>
2022-03-03 20:01:23 +0100

    added start year 2021

---
+++ year2021/cs/Day1.Tests/Tests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Day1.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ year2021/cs/Day1/Program.cs
@@ -0,0 +1,5 @@
+﻿string v = File.ReadAllText("./day13_data_test.txt");
+string[] vs = v.Split("\r\n\r\n");
+
+
+var hej = "hej";
