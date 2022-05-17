e859de4c73919d30f7a2989403ea4e692ade1644
3schwartz <s_schwartz@live.dk>
2022-05-17 17:47:33 +0200

    Int power

--- year2021/cs/Day20.Tests/Day20Tests.cs
+++ year2021/cs/Day20.Tests/Day20Tests.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Numerics;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,9 +12,38 @@ public partial class Day20Tests
     public async Task Test1Async()
     {
         var lines = await File.ReadAllLinesAsync("../../../../../data/day20_data_test.txt");
-
-
-
-
     }
+
+    [Fact]
+    public void CreateBinaryFromSpan()
+    {
+        Span<int> ints = stackalloc int[9] {0, 0, 0, 0, 0, 0, 0, 1, 1};
+        var value = 0;
+
+        for (var i = 0; i < ints.Length; i++)
+        {
+            if (ints[^(i + 1)] == 1)
+            {
+                value += Power(2,i);
+            }
+        }
+    }
+
+    private static int Power(int value, int power)
+    {
+        if (power == 0)
+        {
+            return 1;
+        }
+
+        var final = 1;
+        while (power > 0)
+        {
+            final *= value;
+            power--;
+        }
+
+        return final;
+    }
+
 }
