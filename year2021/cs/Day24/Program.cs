c88df8f0ec1aa9a309720c733a96b268c5aacc40
Soren Schwartz <s_schwartz@live.dk>
2022-06-12 19:50:14 +0200

    Update Program.cs

--- year2021/cs/Day24/Program.cs
+++ year2021/cs/Day24/Program.cs
@@ -1,9 +1,33 @@
 ﻿using Day24;
 
-var alu = new ArithmeticLogicUnit();
+//var alu = new ArithmeticLogicUnit();
+
+//var lines = await File.ReadAllLinesAsync("../../../../../data/day24_data.txt");
+
+//var highstMonad = alu.FindHighestMonad(lines);
+
+//Console.WriteLine($"Part 1: {highstMonad.Highest}");
+
+
+// Thanks: https://github.com/dphilipson/advent-of-code-2021/blob/master/src/days/day24.rs
 
 var lines = await File.ReadAllLinesAsync("../../../../../data/day24_data.txt");
 
-var highstMonad = alu.FindHighestMonad(lines);
+for (int i = 0; i < lines.Length; i++)
+{
+    switch (i % 18)
+    {
+        case 0:
+            Console.WriteLine($"#### Repeat: {i / 18} #####");
+            break;
+        case 5:
+            Console.WriteLine($"x add: {lines[i]}");
+            break;
+        case 15:
+            Console.WriteLine($"y add: {lines[i]}");
+            break;
+        default:
+            break;
+    }
 
-Console.WriteLine($"Part 1: {highstMonad.Highest}");
+}
