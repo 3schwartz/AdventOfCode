d8203a21e15b9f973f082d3053d55cce8667c46e
Soren Schwartz <s_schwartz@live.dk>
2022-06-21 20:11:52 +0200

    day6 2019 part2

--- year2019/cs/Day6.Tests/Day6Tests.cs
+++ year2019/cs/Day6.Tests/Day6Tests.cs
@@ -23,6 +23,27 @@ public class Day6Tests
         int orbits = orbitMap.GetOrbitCount();
 
         output.WriteLine($"Part 1: {orbits}");
+
+        var orbitCalculator = new OrbitDistanceCalculator(lines);
+
+        var distance = orbitCalculator.FindMinimalDistance("YOU", "SAN");
+
+        output.WriteLine($"Part 2: {distance}");
+    }
+
+    [Fact]
+    public void WhenFindShortestBetweenTwoCenter_ThenCorrectMinimal()
+    {
+        // Arrange
+        var lines = File.ReadAllLines("../../../../../data/day6_data_test2.txt");
+        var orbitCalculator = new OrbitDistanceCalculator(lines);
+
+        // Act
+        var distance = orbitCalculator.FindMinimalDistance("YOU", "SAN");
+
+        // Assert
+        Assert.NotNull(distance);
+        Assert.Equal(4, distance);
     }
 
     [Fact]
@@ -37,6 +58,65 @@ public class Day6Tests
 
         // Assert
         Assert.Equal(42, orbits);
+    }
+}
+
+internal class OrbitDistanceCalculator
+{
+    private readonly IDictionary<string, IList<string>> orbits;
+
+    public OrbitDistanceCalculator(string[] lines)
+    {
+        orbits = new Dictionary<string, IList<string>>();
+        foreach (var line in lines)
+        {
+            var split = line.Split(")");
+            AddOrUpdate(split[0], split[1]);
+            AddOrUpdate(split[1], split[0]);
+        }
+    }
+
+
+    internal int? FindMinimalDistance(string from, string to)
+    {
+        var visited = new HashSet<string>();
+        return LookInOrbits(from, to, -1, visited).Distance;
+    }
+
+    private Result LookInOrbits(string from, string to, int debt, ISet<string> visited)
+    {
+        visited.Add(from);
+        if (orbits.TryGetValue(from, out var arounds))
+        {
+            foreach (var around in arounds)
+            {
+                if (visited.Contains(around)) continue;
+
+                if (around == to)
+                {
+                    return new Result(true, debt);
+                }
+
+                var innerLookup = LookInOrbits(around, to, debt + 1, visited);
+                if (innerLookup.Found)
+                {
+                    return innerLookup;
+                }
+            }
+        };
+        return new Result(false, null);
+    }
+
+    private record struct Result(bool Found, int? Distance);
+
+    private void AddOrUpdate(string center, string orbit)
+    {
+        if (orbits.ContainsKey(center))
+        {
+            orbits[center].Add(orbit);
+            return;
+        }
+        orbits[center] = new List<string> { orbit };
     }
 }
 
