633ae27b2d1c226ab16919a86eac3906335fa864
Soren Schwartz <s_schwartz@live.dk>
2022-07-31 10:05:05 +0200

    part1 day4 2019

--- year2019/go/cmd/day4/day4_test.go
+++ year2019/go/cmd/day4/day4_test.go
@@ -10,18 +10,6 @@ import (
 
 type validator func(password int) bool
 
-// func increasingValidator(password int) bool {
-// 	valid := true
-// 	for {
-// 		valid = valid && (password%10 <= (password/10)%10)
-// 		password = password / 10
-// 		if password < 10 {
-// 			break
-// 		}
-// 	}
-// 	return valid
-// }
-
 func increasingValidator(password int) bool {
 	pass := strconv.Itoa(password)
 	valid := true
@@ -36,6 +24,51 @@ func increasingValidator(password int) bool {
 		valid = valid && first <= second
 	}
 	return valid
+}
+
+func twoSequentiallyEqualValidator(password int) bool {
+	pass := strconv.Itoa(password)
+	valid := false
+	for i := 0; i < len(pass)-1; i++ {
+		// Benchmark below conversion
+		// first := int8(pass[i] - '0')
+		// second := int8(pass[i+1] - '0')
+		firstByte := pass[i] - '0'
+		first := *(*int8)(unsafe.Pointer(&firstByte))
+		secondByte := pass[i+1] - '0'
+		second := *(*int8)(unsafe.Pointer(&secondByte))
+		valid = valid || first == second
+	}
+	return valid
+}
+
+type passwordValidator struct {
+	passwords []int
+}
+
+func newPasswordValidator(from int, to int) passwordValidator {
+	size := 1 + to - from
+	passwords := make([]int, 0, size)
+	for i := from; i <= to; i++ {
+		passwords = append(passwords, i)
+	}
+	return passwordValidator{
+		passwords: passwords,
+	}
+}
+
+func (pv *passwordValidator) getNumberValidPassword(validators []validator) int {
+	sum := 0
+	for _, password := range pv.passwords {
+		valid := true
+		for _, validator := range validators {
+			valid = valid && validator(password)
+		}
+		if valid {
+			sum += 1
+		}
+	}
+	return sum
 }
 
 func Test_increasingValidator(t *testing.T) {
@@ -66,13 +99,58 @@ func Test_increasingValidator(t *testing.T) {
 		})
 	}
 }
-func Test_passwordFinder(t *testing.T) {
-	// data := []struct {
-	// 	name string
-	// 	validators []validator
-	// }
+
+func Test_twoSequentiallyEqualValidator(t *testing.T) {
+	data := []struct {
+		name     string
+		password int
+		expected bool
+	}{
+		{"First",
+			112,
+			true},
+		{
+			"Second",
+			123,
+			false,
+		},
+	}
+	// Arrange
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			// Act
+			actual := twoSequentiallyEqualValidator(d.password)
+
+			// Assert
+			if diff := cmp.Diff(actual, d.expected); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
 }
 
-func Test_passFoo(t *testing.T) {
+func Test_passwordFinder(t *testing.T) {
+	data := []struct {
+		name       string
+		validators []validator
+		expected   int
+	}{
+		{
+			"Part1",
+			[]validator{increasingValidator, twoSequentiallyEqualValidator},
+			931,
+		},
+	}
+	// Arrange
+	passwordValidator := newPasswordValidator(272091, 815432)
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			// Act
+			actual := passwordValidator.getNumberValidPassword(d.validators)
 
+			if diff := cmp.Diff(actual, d.expected); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
 }
