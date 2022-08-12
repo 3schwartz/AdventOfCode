612e2c13d24cd43dec3c0006391c9bee6bb643dd
Soren Schwartz <s_schwartz@live.dk>
2022-08-12 12:09:37 +0200

    Added benchmark

---
+++ year2019/cs/Benchmark/Day12Benchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using Day12.Tests;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    //[SimpleJob(launchCount: 2, warmupCount: 1, targetCount: 2)]
+    public class Day12Benchmark
+    {
+        private string[]? lines;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            lines = File.ReadAllLines("../../../../../../../../../data/day12_test2_data.txt");
+        }
+
+        // Can't use [IterationSetup] since test below 100ms
+        // https://benchmarkdotnet.org/articles/samples/IntroSetupCleanupIteration.html
+        [Benchmark(Baseline = true)]
+        public void SetupWithinEachBenchmark()
+        {
+            var moons = lines!.Select(line => Moon.CreateMoon(line).Moon).ToList();
+            _ = new MoonSimulator(moons!);
+        }
+
+        [Benchmark]
+        public void StepsToGetBackToInitial()
+        {
+            var moons = lines!.Select(line => Moon.CreateMoon(line).Moon).ToList();
+            var simulator = new MoonSimulator(moons!);
+            _ = simulator!.StepsToGetBackToInitial();
+        }
+
+        [Benchmark]
+        public async Task StepsToGetBackToInitialAsync()
+        {
+            var moons = lines!.Select(line => Moon.CreateMoon(line).Moon).ToList();
+            var simulator = new MoonSimulator(moons!);
+            _ = await simulator!.StepsToGetBackToInitialAsync();
+        }
+    }
+}
