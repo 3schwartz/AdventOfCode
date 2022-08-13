aaf446f4dd18f8b025be179ce52d3fda7da5327e
Soren Schwartz <s_schwartz@live.dk>
2022-08-13 09:21:20 +0200

    2019 day 12 part2 go

--- year2019/go/cmd/day12/day12_test.go
+++ year2019/go/cmd/day12/day12_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent/pkg/read"
+	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -10,11 +11,7 @@ import (
 func Test_part1(t *testing.T) {
 	// Arrange
 	data := read.ReadDataAsString("day12")
-	lines := strings.Split(data, "\r\n")
-	moons := make([]*moon, 0, len(lines))
-	for _, line := range lines {
-		moons = append(moons, createNewMoon(line))
-	}
+	moons := createMoonsFromInput(data)
 	simulator := createNewMoonSimulator(moons)
 
 	// Act
@@ -27,6 +24,29 @@ func Test_part1(t *testing.T) {
 	}
 }
 
+func Test_part2Sync(t *testing.T) {
+	// Arrange
+	data := read.ReadDataAsString("day12")
+	moons := createMoonsFromInput(data)
+	simulator := createNewMoonSimulator(moons)
+
+	// Act
+	stepsToInitial := simulator.stepsToGetBackToInitial()
+
+	// Assert
+	if stepsToInitial != 551272644867044 {
+		t.Errorf("Wrong steps to get back: %d", stepsToInitial)
+	}
+}
+
+type Direction int
+
+const (
+	X Direction = iota
+	Y
+	Z
+)
+
 type moonSimulator struct {
 	moons []*moon
 	steps int
@@ -34,6 +54,84 @@ type moonSimulator struct {
 
 func createNewMoonSimulator(moons []*moon) *moonSimulator {
 	return &moonSimulator{moons: moons}
+}
+
+func (ms *moonSimulator) stepsToGetBackToInitial() int64 {
+	steps := make([]int, 0)
+	directions := [3]Direction{X, Y, Z}
+	for _, direction := range directions {
+		moonSteps := ms.findStepsToInitialInDirection(direction)
+		steps = append(steps, moonSteps)
+	}
+	yZ := ms.leastCommonMultiple(int64(steps[1]), int64(steps[2]))
+	xYZ := ms.leastCommonMultiple(int64(steps[0]), yZ)
+	return xYZ
+}
+
+func (ms *moonSimulator) leastCommonMultiple(a int64, b int64) int64 {
+	return a * b / ms.greatestCommonDivisor(a, b)
+}
+
+func (ms *moonSimulator) greatestCommonDivisor(a int64, b int64) int64 {
+	for {
+		if b == 0 {
+			break
+		}
+		t := a
+		a = b
+		b = t % b
+	}
+	return a
+}
+
+func (ms *moonSimulator) findStepsToInitialInDirection(direction Direction) int {
+	initialPositions := ms.getMoonDirectionState(direction)
+	moonSteps := 0
+	velocities := map[int][]int{}
+	for {
+		for i := 0; i < len(ms.moons); i++ {
+			velocities[i] = make([]int, 0)
+			for j := 0; j < len(ms.moons); j++ {
+				if i == j {
+					continue
+				}
+				velocityFromMoonInDirection := ms.moons[i].findVelocityFromMoonInDirection(ms.moons[j], direction)
+				velocities[i] = append(velocities[i], velocityFromMoonInDirection)
+			}
+		}
+		for key, pulls := range velocities {
+			sum := 0
+			for _, p := range pulls {
+				sum += p
+			}
+			ms.moons[key].applyVelocityInDirection(sum, direction)
+		}
+		moonSteps++
+		currentPosition := ms.getMoonDirectionState(direction)
+		if ms.isSameState(initialPositions, currentPosition) {
+			break
+		}
+	}
+
+	return moonSteps
+}
+
+func (ms *moonSimulator) isSameState(initial []directionPosition, current []directionPosition) bool {
+	for i := 0; i < len(initial); i++ {
+		if initial[i] != current[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (ms *moonSimulator) getMoonDirectionState(direction Direction) []directionPosition {
+	positions := make([]directionPosition, 0, len(ms.moons))
+	for _, moon := range ms.moons {
+		directionPosition := moon.getDirectionPosition(direction)
+		positions = append(positions, directionPosition)
+	}
+	return positions
 }
 
 func (ms *moonSimulator) takeSteps(stepCount int) {
@@ -94,6 +192,15 @@ type moon struct {
 	cX, cY, cZ int
 }
 
+func createMoonsFromInput(data string) []*moon {
+	lines := strings.Split(data, "\r\n")
+	moons := make([]*moon, 0, len(lines))
+	for _, line := range lines {
+		moons = append(moons, createNewMoon(line))
+	}
+	return moons
+}
+
 func createNewMoon(line string) *moon {
 	line = strings.TrimPrefix(line, "<")
 	line = strings.TrimSuffix(line, ">")
@@ -121,6 +228,19 @@ func (m *moon) findVelocityFromMoon(moonOther *moon) velocity {
 	return velocity{x, y, z}
 }
 
+func (m *moon) findVelocityFromMoonInDirection(moonOther *moon, direction Direction) int {
+	switch direction {
+	case X:
+		return m.findPullDirection(m.cX, moonOther.cX)
+	case Y:
+		return m.findPullDirection(m.cY, moonOther.cY)
+	case Z:
+		return m.findPullDirection(m.cZ, moonOther.cZ)
+	default:
+		panic(fmt.Sprintf("direction not mapped: %d", direction))
+	}
+}
+
 func (m *moon) findPullDirection(current int, other int) int {
 	if current > other {
 		return -1
@@ -141,10 +261,43 @@ func (m *moon) applyVelocity(moonVelocity velocity) {
 	m.cZ += m.vZ
 }
 
+func (m *moon) applyVelocityInDirection(pull int, direction Direction) {
+	switch direction {
+	case X:
+		m.vX += pull
+		m.cX += m.vX
+	case Y:
+		m.vY += pull
+		m.cY += m.vY
+	case Z:
+		m.vZ += pull
+		m.cZ += m.vZ
+	default:
+		panic(fmt.Sprintf("direction not mapped: %d", direction))
+	}
+}
+
 func (m *moon) getTotalEnergy() int {
 	potentialEnergy := abs(m.cX) + abs(m.cY) + abs(m.cZ)
 	kineticEnergy := abs(m.vX) + abs(m.vY) + abs(m.vZ)
 	return potentialEnergy * kineticEnergy
+}
+
+func (m *moon) getDirectionPosition(direction Direction) directionPosition {
+	switch direction {
+	case X:
+		return directionPosition{m.cX, m.vX}
+	case Y:
+		return directionPosition{m.cY, m.vY}
+	case Z:
+		return directionPosition{m.cZ, m.vZ}
+	default:
+		panic(fmt.Sprintf("direction not mapped: %d", direction))
+	}
+}
+
+type directionPosition struct {
+	coordinate, velocity int
 }
 
 func abs(value int) int {
