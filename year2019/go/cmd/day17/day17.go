e8719c558c60f96854da45ae07925dde42e5317f
Soren Schwartz <s_schwartz@live.dk>
2022-10-02 11:43:36 +0200

    Need to change to ascii

--- year2019/go/cmd/day17/day17.go
+++ year2019/go/cmd/day17/day17.go
@@ -3,7 +3,9 @@ package main
 import (
 	"advent/pkg/coders"
 	"advent/pkg/read"
+	"errors"
 	"fmt"
+	"reflect"
 )
 
 func main() {
@@ -14,7 +16,7 @@ func main() {
 
 	fmt.Printf("Part 1: %d", scaffoldThreshold)
 
-	// intCoder.Print(cameraMap)
+	intCoder.Print(cameraMap)
 
 	robot, position, err := intCoder.GetRobotPosition(cameraMap)
 	if err != nil {
@@ -31,6 +33,8 @@ func main() {
 	fmt.Println()
 	fmt.Println("---")
 
+	fin
+
 	fmt.Println(len("10"))
 
 	testSlice := []int{1, 2, 3}
@@ -43,4 +47,84 @@ func main() {
 	fmt.Println(bar[0])
 	fmt.Println(foo)
 
+}
+
+func findMovementLogic(route []string) (movementLogic, error) {
+	length := len(route)
+	for a := 1; a <= 10; a++ {
+		if a > length {
+			break
+		}
+		aFunc := movementFunction{
+			route[0:a],
+			len(route[0:a]),
+		}
+		for b := 1; b <= 10; b++ {
+			endB := a + b
+			if endB > length {
+				break
+			}
+			bFunc := movementFunction{
+				route[a:endB],
+				len(route[a:endB]),
+			}
+			for c := 1; c <= 10; c++ {
+				endC := a + b + c
+				if endC > length {
+					break
+				}
+				cFunc := movementFunction{
+					route[endB:endC],
+					len(route[endB:endC]),
+				}
+
+				var foundLength int
+				movementLogicResult := movementLogic{}
+				for {
+					if doesFuncFit(route, aFunc, foundLength, length) {
+						movementLogicResult.Routine = append(movementLogicResult.Routine, "A")
+						foundLength += aFunc.length
+						continue
+					}
+					if doesFuncFit(route, bFunc, foundLength, length) {
+						movementLogicResult.Routine = append(movementLogicResult.Routine, "B")
+						foundLength += bFunc.length
+						continue
+					}
+					if doesFuncFit(route, cFunc, foundLength, length) {
+						movementLogicResult.Routine = append(movementLogicResult.Routine, "C")
+						foundLength += cFunc.length
+						continue
+					}
+					if foundLength >= length {
+						movementLogicResult.A = aFunc.input
+						movementLogicResult.B = bFunc.input
+						movementLogicResult.C = cFunc.input
+						return movementLogicResult, nil
+					}
+					break
+				}
+			}
+		}
+	}
+	return movementLogic{}, errors.New("not able to find movement logic")
+}
+
+func doesFuncFit(route []string, moveFunc movementFunction, foundLength int, length int) bool {
+	if foundLength+moveFunc.length <= length && reflect.DeepEqual(moveFunc.input, route[foundLength:foundLength+moveFunc.length]) {
+		return true
+	}
+	return false
+}
+
+type movementFunction struct {
+	input  []string
+	length int
+}
+
+type movementLogic struct {
+	Routine []string
+	A       []string
+	B       []string
+	C       []string
 }
--- year2019/go/cmd/day17/day17_test.go
+++ year2019/go/cmd/day17/day17_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"advent/pkg/coders"
 	"advent/pkg/read"
-	"errors"
 	"reflect"
 	"strings"
 	"testing"
@@ -60,84 +59,4 @@ func Test_givenRoute_WhenFindingRoutineAndFunctions_ThenCorrect(t *testing.T) {
 	if diff := cmp.Diff(expectedMovementLogic, movementLogic); diff != "" {
 		t.Error(diff)
 	}
-}
-
-func findMovementLogic(route []string) (movementLogic, error) {
-	length := len(route)
-	for a := 1; a <= 10; a++ {
-		if a > length {
-			break
-		}
-		aFunc := movementFunction{
-			route[0:a],
-			len(route[0:a]),
-		}
-		for b := 1; b <= 10; b++ {
-			endB := a + b
-			if endB > length {
-				break
-			}
-			bFunc := movementFunction{
-				route[a:endB],
-				len(route[a:endB]),
-			}
-			for c := 1; c <= 10; c++ {
-				endC := a + b + c
-				if endC > length {
-					break
-				}
-				cFunc := movementFunction{
-					route[endB:endC],
-					len(route[endB:endC]),
-				}
-
-				var foundLength int
-				movementLogicResult := movementLogic{}
-				for {
-					if doesFuncFit(route, aFunc, foundLength, length) {
-						movementLogicResult.Routine = append(movementLogicResult.Routine, "A")
-						foundLength += aFunc.length
-						continue
-					}
-					if doesFuncFit(route, bFunc, foundLength, length) {
-						movementLogicResult.Routine = append(movementLogicResult.Routine, "B")
-						foundLength += bFunc.length
-						continue
-					}
-					if doesFuncFit(route, cFunc, foundLength, length) {
-						movementLogicResult.Routine = append(movementLogicResult.Routine, "C")
-						foundLength += cFunc.length
-						continue
-					}
-					if foundLength >= length {
-						movementLogicResult.A = aFunc.input
-						movementLogicResult.B = bFunc.input
-						movementLogicResult.C = cFunc.input
-						return movementLogicResult, nil
-					}
-					break
-				}
-			}
-		}
-	}
-	return movementLogic{}, errors.New("not able to find movement logic")
-}
-
-func doesFuncFit(route []string, moveFunc movementFunction, foundLength int, length int) bool {
-	if foundLength+moveFunc.length <= length && reflect.DeepEqual(moveFunc.input, route[foundLength:foundLength+moveFunc.length]) {
-		return true
-	}
-	return false
-}
-
-type movementFunction struct {
-	input  []string
-	length int
-}
-
-type movementLogic struct {
-	Routine []string
-	A       []string
-	B       []string
-	C       []string
 }
