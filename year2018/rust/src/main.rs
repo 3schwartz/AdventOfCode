e3846497f52b90081d3c1548d8e70175f7e1fa79
3schwartz <s_schwartz@live.dk>
2022-11-01 16:27:05 +0100

    Clean

--- year2018/rust/src/main.rs
+++ year2018/rust/src/main.rs
@@ -1,17 +1,7 @@
-use std::{env, fs};
-use std::path::Path;
-use relative_path::RelativePath;
+use std::{fs};
 
-fn main() -> std::io::Result<()> {
-    let root  = env::current_dir()?;
-    println!("The current directory is {}", root.display());
-
-    let relativePath = RelativePath::new("../data/day1_data.txt");
-    let fullPath = relativePath.to_path(&root);
-
-    println!("The full path is {}", fullPath.display());
-
-    let file = match fs::read_to_string(fullPath) {
+fn main() {
+    let file = match fs::read_to_string("../data/day1_data.txt") {
         Err(why) => panic!("couldn't open file: {}", why),
         Ok(file) => file
     };
@@ -20,21 +10,15 @@ fn main() -> std::io::Result<()> {
 
     let mut sum :i32 = 0;
     for s in lines {
-        let numberString = &s[1..];
-        let mut foo = numberString.parse();
-        let mut number : i32 = foo.unwrap();
+        let number_string = &s[1..];
+        let mut number : i32 = number_string.parse().unwrap();
         let sign = s.chars().next().unwrap();
         match sign {
-            '+'=> {
-                number = number
-            },
-            '-'=> {
-                number = -1*number
-            }
+            '+'=> number = number,
+            '-'=> number = -1*number,
             _ => println!("{} not known", sign)
         }
         sum+= number;
     }
     println!("Part 1: {}", sum);
-    Ok(())
 }
