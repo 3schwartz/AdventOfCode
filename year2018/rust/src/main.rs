634ea2b0ae177d3df32ad5a6c50f280141e11408
3schwartz <s_schwartz@live.dk>
2022-11-01 16:39:31 +0100

    2018 Day1 Part 2

--- year2018/rust/src/main.rs
+++ year2018/rust/src/main.rs
@@ -1,4 +1,5 @@
 use std::{fs};
+use std::collections::HashSet;
 
 fn main() {
     let file = match fs::read_to_string("../data/day1_data.txt") {
@@ -9,7 +10,7 @@ fn main() {
     let lines = file.split("\r\n");
 
     let mut sum :i32 = 0;
-    for s in lines {
+    for s in lines.clone() {
         let number_string = &s[1..];
         let mut number : i32 = number_string.parse().unwrap();
         let sign = s.chars().next().unwrap();
@@ -21,4 +22,31 @@ fn main() {
         sum+= number;
     }
     println!("Part 1: {}", sum);
+
+    let mut visited = HashSet::new();
+    sum = 0;
+    let mut found = false;
+    loop {
+        for s in lines.clone() {
+            let number_string = &s[1..];
+            let mut number : i32 = number_string.parse().unwrap();
+            let sign = s.chars().next().unwrap();
+            match sign {
+                '+'=> number = number,
+                '-'=> number = -1*number,
+                _ => println!("{} not known", sign)
+            }
+            sum+= number;
+
+            if visited.contains(&sum) {
+                found = true;
+                break
+            }
+            visited.insert(sum);
+        }
+        if found {
+            break
+        }
+    }
+    println!("Part 2: {}", sum);
 }
