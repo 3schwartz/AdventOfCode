19907e922a0c9772365eca5b5d03ea5cad5b065b
Soren Schwartz <s_schwartz@live.dk>
2022-11-18 22:14:13 +0100

    refactor

--- year2018/rust/day4/src/main.rs
+++ year2018/rust/day4/src/main.rs
@@ -1,58 +1,23 @@
 use std::{fs, collections::HashMap};
 
-fn get_date_as_type(line : &str) -> (i16, i8, i8, i8, i8){
-    let (first, _) = line.split_at(18);
-    let date = first.trim_matches(|c| c == '[' || c == ']');
-    let (date_part, time_part) = date.split_at(10);
-    let date_parts : Vec<&str>= date_part.split("-").collect();
-
-    let time_parts = time_part.split(":").collect::<Vec<&str>>();
-
-    (
-        date_parts[0].parse::<i16>().expect("not able to parse year"),
-        date_parts[1].parse::<i8>().expect("not able to parse month"), 
-        date_parts[2].parse::<i8>().expect("not able to parse date"),
-        time_parts[0].trim().parse::<i8>().expect("not able to parse hour"),
-        time_parts[1].parse::<i8>().expect("not able to parse minute")
-    )
-}
-
-
 fn main() {
     let file: String = fs::read_to_string("../../data/day4_data.txt")
         .expect("couldn't open file");
     let lines = file.split("\r\n");
-    let lines_iter : Vec<&str> = lines.collect();
+    let mut lines_iter : Vec<&str> = lines.collect();
+
+    sort_lines(&mut lines_iter);
+    print_lines(&lines_iter);
 
     let mut current_id = -1;
-    let mut last_hour = -1;
-    // let mut is_sleeping : bool = false;
+    let mut last_minute = -1;
+    let mut guard_map: HashMap<i32, HashMap<i16, i32>> = HashMap::new();
 
-    let mut guardMap: HashMap<i32, HashMap<i8, i32>> = HashMap::new();
-
-    assert_eq!("1foo1barXX".trim_matches(|c| c == '1' || c == 'X'), "foo1bar");
-
-    let mut foo = lines_iter.clone();
-    foo.sort_by(|a, b| {
-        let first_timestamp = get_date_as_type(a);
-        let second_timestamp = get_date_as_type(b);
-
-        first_timestamp.0.cmp(&second_timestamp.0)
-            .then(first_timestamp.1.cmp(&second_timestamp.1))
-            .then(first_timestamp.2.cmp(&second_timestamp.2))
-            .then(first_timestamp.3.cmp(&second_timestamp.3))
-            .then(first_timestamp.4.cmp(&second_timestamp.4))
-    });
-
-    for line in &foo {
-        println!("{}", line)
-    }
-
-    for line in &foo {
+    for line in &lines_iter {
         let time_and_instruction: Vec<&str> = line.split("] ")
             .collect();
 
-        let (year, month, day, hour, minute) = get_date_as_type(line);
+        let (_, _, _, _, minute) = get_date_as_type(line);
         
         match time_and_instruction[1].chars().next().unwrap() {
             'G' => {
@@ -62,47 +27,69 @@ fn main() {
                 let id : i32 = index[0].parse()
                     .expect("being able to parse");
 
-                guardMap.entry(id).or_insert(HashMap::new());
+                guard_map.entry(id).or_insert(HashMap::new());
                 current_id = id;
                 
             },
-            'f' => {
-                // is_sleeping = true;
-            },
             'w' => {
-                // if !is_sleeping {
-                //     break;
-                // }
-                let entry = guardMap.entry(current_id).or_insert(HashMap::new());
-                let mut first_hour = last_hour;
+                let entry = guard_map.entry(current_id).or_insert(HashMap::new());
+                let mut first_minute = last_minute;
 
-                if last_hour >= hour {
-                    for h in last_hour..= 60 {
+                if last_minute >= minute {
+                    for h in last_minute..= 60 {
                         entry.entry(h)
                             .and_modify(|counter| *counter += 1)
                             .or_insert(1);
                     }
-                    first_hour = 0;
+                    first_minute = 0;
 
                 }
 
-                for h in first_hour..hour {
+                for h in first_minute..minute {
                     entry.entry(h)
                         .and_modify(|counter| *counter += 1)
                         .or_insert(1);
                 }
             }
-            _ => println!("not known")
+            _ => ()
         }
 
-        last_hour = hour;
+        last_minute = minute;
     }
 
+    let id_max = find_max_id(&guard_map);
+    let max_minute = find_max_minute(id_max, &guard_map);
+
+    println!("Part 1: {}", id_max * max_minute)
+
+
+}
+
+fn print_lines(lines : &Vec<&str>) -> () {
+    for line in lines {
+        println!("{}", line)
+    }
+}
+
+fn sort_lines(lines: &mut Vec<&str>) -> () {
+    lines.sort_by(|a, b| {
+        let first_timestamp = get_date_as_type(a);
+        let second_timestamp = get_date_as_type(b);
+
+        first_timestamp.0.cmp(&second_timestamp.0)
+            .then(first_timestamp.1.cmp(&second_timestamp.1))
+            .then(first_timestamp.2.cmp(&second_timestamp.2))
+            .then(first_timestamp.3.cmp(&second_timestamp.3))
+            .then(first_timestamp.4.cmp(&second_timestamp.4))
+    });
+}
+
+fn find_max_id(guard_map : &HashMap<i32, HashMap<i16, i32>>) -> i32 {
     let mut max = 0;
     let mut id_max = 0;
-    for (id, hours) in &guardMap {
+    for (id, minute) in guard_map {
         let mut sum = 0;
-        for (_, count) in hours {
+        for (_, count) in minute {
             sum += count;
         }
         if sum > max {
@@ -110,16 +97,36 @@ fn main() {
             id_max = *id;
         }
     }
+    id_max
+}
+
+fn find_max_minute(id_max : i32, guard_map : &HashMap<i32, HashMap<i16, i32>>) -> i32 {
+    let max_id_entry = guard_map.get(&id_max)
+        .expect("should exist");
     let mut max_count = 0;
-    let mut max_hour : i8 = -1;
-    for (h, count) in guardMap.entry(id_max).or_default() {
+    let mut max_minute = -1;
+    for (h, count) in max_id_entry {
         if *count > max_count {
             max_count = *count;
-            max_hour = *h;
+            max_minute = *h;
         }
     }
+    max_minute as i32
+}
 
-    println!("Part 1: {}", max_hour)
+fn get_date_as_type(line : &str) -> (i16, i16, i16, i16, i16){
+    let (first, _) = line.split_at(18);
+    let date = first.trim_matches(|c| c == '[' || c == ']');
+    let (date_part, time_part) = date.split_at(10);
+    let date_parts : Vec<&str>= date_part.split("-").collect();
 
+    let time_parts = time_part.split(":").collect::<Vec<&str>>();
 
+    (
+        date_parts[0].parse::<i16>().expect("not able to parse year"),
+        date_parts[1].parse::<i16>().expect("not able to parse month"), 
+        date_parts[2].parse::<i16>().expect("not able to parse date"),
+        time_parts[0].trim().parse::<i16>().expect("not able to parse hour"),
+        time_parts[1].parse::<i16>().expect("not able to parse minute")
+    )
 }
