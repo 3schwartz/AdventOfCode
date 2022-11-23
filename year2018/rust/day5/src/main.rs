696beb4719d8df2dc6da9094fd37a21f6d3629b3
Soren Schwartz <s_schwartz@live.dk>
2022-11-23 21:40:29 +0100

    Avoid Rc and RefCell

--- year2018/rust/day5/src/main.rs
+++ year2018/rust/day5/src/main.rs
@@ -1,105 +1,96 @@
-use std::{cell::RefCell, rc::{Weak, Rc}, borrow::{BorrowMut}};
+
 
 fn main() {
     let input = "dabAcCaCBAcCcaDA";
     let mut chars = input.chars();
     let c = chars.next().unwrap();
     
-    let mut polymer = Polymer::new(Unit::new(c));
+    let mut first = UnitP::new(c);
 
     for c in chars {
-        polymer.insert(c)
+        first.append(c);
     }
 
-    polymer.react();
+    let result = match first.react(){
+        (None, _) => panic!("Didn't work"),
+        (Some(unit), _) => unit,
+    };
+
+    let length = result.get_length();
+
+    println!("Part 1: {}", length)
 
 }
-// type UnitRef = Rc<RefCell<Unit>>;
-// type UnitWeakRef = Weak<RefCell<Unit>>;
 
-struct Polymer {
-    root: Rc<RefCell<Unit>>,
-    previous: Rc<RefCell<Unit>>
-}
-
-impl Polymer {
-    fn new(first: Rc<RefCell<Unit>>) -> Self {
-        Self {
-            previous: first.clone(),
-            root: first,
-        }
-    }
-
-    fn insert(&mut self, character: char) {
-        let next_unit = Unit::new_with_previous(character, &self.previous);
-        {
-            let mut previous_unit = self.previous.borrow();
-            previous_unit.next= Some(next_unit.clone());
-        }
-        self.previous = next_unit;
-    }
-
-    fn react(&self) {
-        let root_borrowed = self.root.borrow();
-        let length = root_borrowed.get_length();
-        root_borrowed.react();
-    }
-}
-
-struct Unit {
+struct UnitP {
     character: char,
-    previous: Option<Rc<RefCell<Unit>>>,
-    next: Option<Rc<RefCell<Unit>>>,
+    next: Option<Box<UnitP>>,
 }
 
-impl Unit {
-    fn new(character: char) -> Rc<RefCell<Unit>> {
-        Rc::new(RefCell::new(Self { 
-            character,
-            previous: None,
-            next: None
-        }))
+impl UnitP {
+    fn new (character: char) -> Self {
+        Self {
+            character, next: None }
     }
 
-    fn new_with_previous(character: char, previous: &Rc<RefCell<Unit>>) -> Rc<RefCell<Unit>> {
-        Rc::new(RefCell::new(Self { 
-            character,
-            previous: Some(previous.clone()),
-            next: None
-        }))
-    }
-
-    fn react(&mut self) {
-        match &self.next {
+    fn append(&mut self, character: char) {
+        match &mut self.next {
             Some(next) => {
-                let next_borrow = next.borrow();
-
-                if (next_borrow.character.is_lowercase() && self.character.is_uppercase() ||
-                    next_borrow.character.is_uppercase() && self.character.is_lowercase()) &&
-                    self.character.to_lowercase().eq(next_borrow.character.to_lowercase()) {
-                    
-                    match &mut self.previous {
-                        Some(previous) => {
-                            let mut previous_borrow = previous.borrow();
-                            previous_borrow.next = self.next;
-                        },
-                        None => todo!(),
-                    }
-                }
+                next.append(character);
             },
-            None => return,
+            None => {
+                self.next = Some(Box::new(UnitP::new(character)));
+            },
         }
+    }
+
+    fn react(mut self) -> (Option<Box<UnitP>>, bool) {
+        let next_temp = self.next;
+        match next_temp {
+            Some(next) if (self.characters_match(&next)) => {
+                    match next.next {
+                        Some(new_next) => (new_next.react().0, true),
+                        None => (None, true),
+                    }
+                },
+            Some(next) => {
+                let foo = next.react();
+
+                if foo.1 {
+                    self.next = foo.0;
+                    return self.react();
+                }
+
+                return (Some(Box::new(self)), false);
+
+
+                // match next.react() {
+                //     (new_next, true) => {
+                //         self.next = new_next;
+                //         return self.react();
+                //     },
+                //     (_, false) => {
+                //         return (Some(Box::new(self)), false)
+                //     }
+                // }
+            },
+            None => (Some(Box::new(self)), false)
+        }
+    }
+
+    fn characters_match(&self, other : &UnitP) -> bool {
+        (other.character.is_ascii_lowercase() && self.character.is_ascii_uppercase() ||
+        other.character.is_ascii_uppercase() && self.character.is_ascii_lowercase()) &&
+            self.character.to_ascii_lowercase() == other.character.to_ascii_lowercase()
     }
 
     fn get_length(&self) -> i32 {
         let sum = match &self.next {
             Some(next) => {
-                let next_borrowed = next.borrow();
-                next_borrowed.get_length() + 1
+                next.get_length() + 1
             },
             None => 1,
         };
         sum
     }
 }
-
