08cb023e7c14cac69309c0ea5f978e3ff4f9e764
Soren Schwartz <s_schwartz@live.dk>
2022-11-23 16:06:28 +0100

    2018 day5 part2 try with Rc and RefCell

--- year2018/rust/day5/src/main.rs
+++ year2018/rust/day5/src/main.rs
@@ -1,47 +1,54 @@
-use std::{cell::RefCell, rc::{Weak, Rc}};
+use std::{cell::RefCell, rc::{Weak, Rc}, borrow::{BorrowMut}};
 
 fn main() {
     let input = "dabAcCaCBAcCcaDA";
     let mut chars = input.chars();
     let c = chars.next().unwrap();
     
-    let mut polymer = Polymer::new();
-    polymer.insert_first(Unit::new(c));
+    let mut polymer = Polymer::new(Unit::new(c));
 
     for c in chars {
-        // previous = previous.add_next(c);
-        // println!("{}", previous.character)
+        polymer.insert(c)
     }
+
+    polymer.react();
 
 }
 // type UnitRef = Rc<RefCell<Unit>>;
 // type UnitWeakRef = Weak<RefCell<Unit>>;
 
 struct Polymer {
-    first: Option<Rc<RefCell<Unit>>>
+    root: Rc<RefCell<Unit>>,
+    previous: Rc<RefCell<Unit>>
 }
 
 impl Polymer {
-    fn new() -> Self {
+    fn new(first: Rc<RefCell<Unit>>) -> Self {
         Self {
-            first: None
+            previous: first.clone(),
+            root: first,
         }
     }
 
-    fn insert_first(&mut self, unit: Rc<RefCell<Unit>>) {
-        self.first = Some(unit)
+    fn insert(&mut self, character: char) {
+        let next_unit = Unit::new_with_previous(character, &self.previous);
+        {
+            let mut previous_unit = self.previous.borrow();
+            previous_unit.next= Some(next_unit.clone());
+        }
+        self.previous = next_unit;
     }
 
-    fn insert(&self, character: char, unit: &Rc<RefCell<Unit>>) {
-        let next_unit = Unit::new_with_previous(character, unit);
-        let mut previous_unit = unit.borrow_mut();
-        previous_unit.next = Some(next_unit);
+    fn react(&self) {
+        let root_borrowed = self.root.borrow();
+        let length = root_borrowed.get_length();
+        root_borrowed.react();
     }
 }
 
 struct Unit {
     character: char,
-    previous: Option<Weak<RefCell<Unit>>>,
+    previous: Option<Rc<RefCell<Unit>>>,
     next: Option<Rc<RefCell<Unit>>>,
 }
 
@@ -57,9 +64,42 @@ impl Unit {
     fn new_with_previous(character: char, previous: &Rc<RefCell<Unit>>) -> Rc<RefCell<Unit>> {
         Rc::new(RefCell::new(Self { 
             character,
-            previous: Some(Rc::<RefCell<Unit>>::downgrade(previous)),
+            previous: Some(previous.clone()),
             next: None
         }))
+    }
+
+    fn react(&mut self) {
+        match &self.next {
+            Some(next) => {
+                let next_borrow = next.borrow();
+
+                if (next_borrow.character.is_lowercase() && self.character.is_uppercase() ||
+                    next_borrow.character.is_uppercase() && self.character.is_lowercase()) &&
+                    self.character.to_lowercase().eq(next_borrow.character.to_lowercase()) {
+                    
+                    match &mut self.previous {
+                        Some(previous) => {
+                            let mut previous_borrow = previous.borrow();
+                            previous_borrow.next = self.next;
+                        },
+                        None => todo!(),
+                    }
+                }
+            },
+            None => return,
+        }
+    }
+
+    fn get_length(&self) -> i32 {
+        let sum = match &self.next {
+            Some(next) => {
+                let next_borrowed = next.borrow();
+                next_borrowed.get_length() + 1
+            },
+            None => 1,
+        };
+        sum
     }
 }
 
