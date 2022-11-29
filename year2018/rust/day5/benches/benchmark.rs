5e818e00f56515433de7ca461e873dfe006d763a
Soren Schwartz <s_schwartz@live.dk>
2022-11-29 20:54:16 +0100

    2018 day5 benchmark all

--- year2018/rust/day5/benches/benchmark.rs
+++ year2018/rust/day5/benches/benchmark.rs
@@ -1,19 +1,48 @@
+use std::time::Duration;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use day5::simple;
 use day5::value;
 
-pub fn simple_benchmark(c: &mut Criterion) {
-    let input = "dabAcCaCBAcCcaDA";
-    c.bench_function("Simple", |b| b.iter(|| {
+// pub fn simple_small_benchmark(c: &mut Criterion) {
+//     let input = "dabAcCaCBAcCcaDA";
+//     c.bench_function("Simple - Small", |b| b.iter(|| {
+//         let mut polymer = simple::Polymer::new(&input);
+//         black_box(polymer.find_polymer_length());
+//     }));
+// }
+
+// pub fn value_small_benchmark(c: &mut Criterion) {
+//     let input = "dabAcCaCBAcCcaDA";
+//     c.bench_function("Value - Small", |b| b.iter(|| black_box(value::get_polymer_length(input))));
+// }
+
+/// All
+pub fn all_benchmark(c: &mut Criterion) {
+    let input = std::fs::read_to_string("../../data/day5_data.txt")
+                .expect("couldn't open file");
+
+    let mut group = c.benchmark_group("All");
+    group.sample_size(10);
+    group.measurement_time(Duration::new(120, 0));
+
+    group.bench_function("Simple - All", |b| b.iter(|| {
         let mut polymer = simple::Polymer::new(&input);
         black_box(polymer.find_polymer_length());
     }));
+    group.bench_function("Value - All", |b| b.iter(|| black_box(value::get_polymer_length(&input))));
+
+    group.finish();
 }
 
-pub fn value_benchmark(c: &mut Criterion) {
-    let input = "dabAcCaCBAcCcaDA";
-    c.bench_function("Value", |b| b.iter(|| black_box(value::get_polymer_length(input))));
+/// All
+pub fn value_all_benchmark(c: &mut Criterion) {
+    let input = std::fs::read_to_string("../../data/day5_data.txt")
+                .expect("couldn't open file");
+    c.bench_function("Value - All", |b| b.iter(|| black_box(value::get_polymer_length(&input))));
 }
 
-criterion_group!(benches, simple_benchmark, value_benchmark);
+criterion_group!(benches, 
+    // simple_small_benchmark, value_small_benchmark,
+    all_benchmark);
 criterion_main!(benches);
