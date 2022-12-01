899283dc8fef36a55ff18c753f0d890222da9935
Soren Schwartz <s_schwartz@live.dk>
2022-12-01 07:25:59 +0100

    2022 day1 refactor

--- year2022/go22/cmd/day1/day1.go
+++ year2022/go22/cmd/day1/day1.go
@@ -10,23 +10,9 @@ import (
 
 func main() {
 	input := readData()
-	elves := strings.Split(input, "\r\n\r\n")
-	sums := make([]int, len(elves))
-	for _, elf := range elves {
-		currentSum := 0
-		calories := strings.Split(elf, "\r\n")
-		for _, calorie := range calories {
-			i, err := strconv.Atoi(calorie)
-			if err != nil {
-				panic(err)
-			}
-			currentSum += i
-		}
-		sums = append(sums, currentSum)
-	}
-	sort.SliceStable(sums, func(i, j int) bool {
-		return sums[i] > sums[j]
-	})
+	p := picnic{}
+	elves := p.elfCreator(input)
+	sums := p.getSortedElfCalorieSums(elves)
 	maxSum := sums[0]
 	fmt.Printf("Part 1: %d\n", maxSum)
 
@@ -35,6 +21,46 @@ func main() {
 		threeHighest += i
 	}
 	fmt.Printf("Part 2: %d\n", threeHighest)
+}
+
+type elf struct {
+	calories string
+}
+
+func (e elf) getCalorieSum() int {
+	currentSum := 0
+	calories := strings.Split(e.calories, "\r\n")
+	for _, calorie := range calories {
+		i, err := strconv.Atoi(calorie)
+		if err != nil {
+			panic(err)
+		}
+		currentSum += i
+	}
+	return currentSum
+}
+
+type picnic struct{}
+
+func (p picnic) elfCreator(input string) []elf {
+	elvesInput := strings.Split(input, "\r\n\r\n")
+	elves := make([]elf, len(elvesInput))
+	for i, elfInput := range elvesInput {
+		elves[i] = elf{elfInput}
+	}
+	return elves
+}
+
+func (p picnic) getSortedElfCalorieSums(elves []elf) []int {
+	sums := make([]int, len(elves))
+	for _, elf := range elves {
+		currentSum := elf.getCalorieSum()
+		sums = append(sums, currentSum)
+	}
+	sort.SliceStable(sums, func(i, j int) bool {
+		return sums[i] > sums[j]
+	})
+	return sums
 }
 
 func readData() string {
