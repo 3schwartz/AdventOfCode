e6d4843437f0a690c7bca307e09fecb588324762
Soren Schwartz <s_schwartz@live.dk>
2022-12-01 07:17:56 +0100

    2022 day1

--- year2022/go22/cmd/day1/day1.go
+++ year2022/go22/cmd/day1/day1.go
@@ -3,14 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	input := readData()
+	elves := strings.Split(input, "\r\n\r\n")
+	sums := make([]int, len(elves))
+	for _, elf := range elves {
+		currentSum := 0
+		calories := strings.Split(elf, "\r\n")
+		for _, calorie := range calories {
+			i, err := strconv.Atoi(calorie)
+			if err != nil {
+				panic(err)
+			}
+			currentSum += i
+		}
+		sums = append(sums, currentSum)
+	}
+	sort.SliceStable(sums, func(i, j int) bool {
+		return sums[i] > sums[j]
+	})
+	maxSum := sums[0]
+	fmt.Printf("Part 1: %d\n", maxSum)
+
+	threeHighest := 0
+	for _, i := range sums[0:3] {
+		threeHighest += i
+	}
+	fmt.Printf("Part 2: %d\n", threeHighest)
 }
 
 func readData() string {
-	f, err := os.ReadFile("../../../data/test.txt")
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(path)
+	f, err := os.ReadFile("../../../data/day1_data.txt")
 	if err != nil {
 		panic(err)
 	}
