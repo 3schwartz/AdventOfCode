61a802b14d6a6b082eb0c7ece6d63d497f0a296c
Soren Schwartz <s_schwartz@live.dk>
2022-12-12 10:39:00 +0100

    2022 day12 part2

--- year2022/go22/cmd/day12/day12.go
+++ year2022/go22/cmd/day12/day12.go
@@ -4,14 +4,19 @@ import (
 	"advent2022/pkg/collections"
 	"advent2022/pkg/io"
 	"fmt"
+	"math"
 	"strings"
 )
 
 func main() {
 	input := io.ReadData("12")
-	steps := findShortestPath(input)
+	steps := singleShortestPath(input)
 
 	fmt.Printf("Part 1: %d\n", steps)
+
+	minSteps := multipleShortestPath(input)
+
+	fmt.Printf("Part 2: %d\n", minSteps)
 }
 
 type coord struct {
@@ -39,13 +44,45 @@ func createHeightMap(input string) (heightMap, coord) {
 	return areaHeightMap, start
 }
 
+func createHeightMapWithStartingPoints(input string) (heightMap, []coord) {
+	areaHeightMap := make(heightMap)
+	startingPoints := make([]coord, 0)
+	for x, line := range strings.Split(input, "\r\n") {
+		for y, height := range line {
+			areaHeightMap[coord{x, y}] = height
+			if height == 'S' || height == 'a' {
+				startingPoints = append(startingPoints, coord{x, y})
+			}
+		}
+	}
+	return areaHeightMap, startingPoints
+}
+
 func (h heightMap) getAdjacent() [4]coord {
 	return [4]coord{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 }
 
-func findShortestPath(input string) int {
-	areaHeightMap, start := createHeightMap(input)
+func multipleShortestPath(input string) int {
+	areaHeightMap, startingPoints := createHeightMapWithStartingPoints(input)
+	stepsMin := math.MaxInt
+	visited := map[coord]struct{}{}
+	for _, start := range startingPoints {
+		steps := findShortestPath(areaHeightMap, start, visited)
+		if steps < stepsMin && steps != 0 {
+			stepsMin = steps
+		}
+		visited[start] = struct{}{}
+	}
+	return stepsMin
+}
 
+func singleShortestPath(input string) int {
+	areaHeightMap, start := createHeightMap(input)
+	steps := findShortestPath(areaHeightMap, start, map[coord]struct{}{})
+	return steps
+}
+
+func findShortestPath(areaHeightMap heightMap, start coord, visitedBefore map[coord]struct{}) int {
 	queue := collections.CreatePriorityMap[coord]()
 	queue.Append(start, 0, make(map[coord]struct{}))
 	adjacent := areaHeightMap.getAdjacent()
@@ -63,6 +100,12 @@ func findShortestPath(input string) int {
 			}
 			if currentHeight == 'S' {
 				currentHeight = 'a'
+			}
+			if len(visitedBefore) > 0 && currentHeight == 'a' {
+				_, ok := visitedBefore[currentCoord]
+				if ok {
+					continue
+				}
 			}
 			for _, adj := range adjacent {
 				_, hasVisited := visited[currentCoord]
--- year2022/go22/cmd/day12/day12_test.go
+++ year2022/go22/cmd/day12/day12_test.go
@@ -7,12 +7,25 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func Test_part2(t *testing.T) {
+	// Arrange
+	input := io.ReadData("12_test")
+
+	// Act
+	actual := multipleShortestPath(input)
+
+	// Assert
+	if diff := cmp.Diff(actual, 29); diff != "" {
+		t.Error(diff)
+	}
+}
+
 func Test_part1(t *testing.T) {
 	// Arrange
 	input := io.ReadData("12_test")
 
 	// Act
-	actual := findShortestPath(input)
+	actual := singleShortestPath(input)
 
 	// Assert
 	if diff := cmp.Diff(actual, 31); diff != "" {
