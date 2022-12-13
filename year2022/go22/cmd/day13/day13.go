eba1d78fd13e1dbe36ad3c9b4f74abc944af525a
Soren Schwartz <s_schwartz@live.dk>
2022-12-13 20:46:06 +0100

    2022 day13 part1

---
+++ year2022/go22/cmd/day13/day13.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"advent2022/pkg/io"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	input := io.ReadData("13")
+	groups := strings.Split(input, "\r\n\r\n")
+
+	sum := 0
+	for i, group := range groups {
+		parts := strings.Split(group, "\r\n")
+		first, _ := createElement(parts[0])
+		second, _ := createElement(parts[1])
+		compare := first.compare(second)
+		if compare == 1 {
+			sum += i + 1
+		}
+	}
+	fmt.Printf("Part 1: %d", sum)
+}
+
+type element struct {
+	isValue  bool
+	isList   bool
+	value    int
+	children []element
+}
+
+func createElement(line string) (element, int) {
+	c := element{children: make([]element, 0)}
+	i := 0
+	for i < len(line) {
+		elm := line[i]
+		i++
+		if elm == ',' || elm == '[' {
+			c.isList = true
+			child, iReturn := createElement(line[i:])
+			if iReturn == 0 {
+				continue
+			}
+			c.children = append(c.children, child)
+			i += iReturn
+			continue
+		}
+		if elm == ']' {
+			break
+		}
+		c.isValue = true
+		c.value = int(elm - '0')
+		break
+	}
+	if !c.isValue && len(c.children) == 0 && !c.isList {
+		i--
+	}
+	return c, i
+}
+
+func (e element) compare(other element) int {
+	if e.isValue && other.isValue {
+		if e.value < other.value {
+			return 1
+		} else if e.value == other.value {
+			return 0
+		} else {
+			return -1
+		}
+	}
+	if !e.isValue && !other.isValue {
+		idx := 0
+		for idx < len(e.children) && idx < len(other.children) {
+			childCompare := e.children[idx].compare(other.children[idx])
+			if childCompare == 1 {
+				return 1
+			}
+			if childCompare == -1 {
+				return -1
+			}
+			idx++
+		}
+		if len(e.children) < len(other.children) {
+			return 1
+		}
+		if len(e.children) > len(other.children) {
+			return -1
+		}
+		return 0
+	}
+	if !e.isValue && other.isValue {
+		return e.compare(element{children: []element{other}})
+	}
+	return element{children: []element{e}}.compare(other)
+}
--- year2022/go22/cmd/day13/day13_test.go
+++ year2022/go22/cmd/day13/day13_test.go
@@ -2,61 +2,11 @@ package main
 
 import (
 	"advent2022/pkg/io"
-	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
-
-type element struct {
-	isList   bool
-	value    int
-	children []element
-}
-
-// func createLine(line string) element {
-// 	for i, elm := range line {
-// 		if elm == '[' {
-// 			c.isValue = false
-// 			getChild := createElement(line[i+1:])
-// 		}
-// 	}
-// }
-
-func createElement(line string) (element, int) {
-	c := element{children: make([]element, 0)}
-	i := 0
-	for i < len(line) {
-		elm := line[i]
-		i++
-		if elm == '[' {
-			c.isList = true
-		}
-		if elm == ',' || elm == '[' {
-			child, iReturn := createElement(line[i:])
-			c.children = append(c.children, child)
-			i += iReturn
-			continue
-		}
-		// if elm == '[' {
-		// 	c.isList = true
-		// 	child, iReturn := createElement(line[i:])
-		// 	c.children = append(c.children, child)
-		// 	i += iReturn
-		// 	continue
-		// }
-		if elm == ']' {
-			break
-		}
-		if c.isList == false {
-			c.value = int(elm - '0')
-			break
-		}
-		if c.isList == true {
-			c.children = append(c.children, element{value: int(elm - '0')})
-		}
-	}
-	return c, i
-}
 
 func Test_whenCreate_ThenCorrect(t *testing.T) {
 	// Arrange
@@ -66,31 +16,58 @@ func Test_whenCreate_ThenCorrect(t *testing.T) {
 	lists, _ := createElement(input)
 
 	// Assert
-	if lists.isList != true {
+	if lists.isValue != false {
 		t.Error("should be list")
 	}
-	if lists.children[0].isList != true && lists.children[0].children[0].value == 1 {
+	if lists.children[0].isValue != false && lists.children[0].children[0].value == 1 {
 		t.Error(lists.children[0])
 	}
-	if lists.children[1].isList != true && lists.children[1].children[0].value == 2 {
+	if lists.children[1].isValue != false && lists.children[1].children[0].value == 2 {
 		t.Error(lists.children[1])
 	}
+}
 
+func Test_whenCreateWithEmpty_ThenCorrect(t *testing.T) {
+	// Arrange
+	input := "[[],[2,3,4]]"
+
+	// Act
+	lists, _ := createElement(input)
+
+	// Assert
+	if len(lists.children[0].children) != 0 {
+		t.Error(len(lists.children[0].children))
+	}
+	if lists.isValue != false {
+		t.Error("should be list")
+	}
+	if lists.children[0].isValue != false && lists.children[0].children[0].isValue == false {
+		t.Error(lists.children[0])
+	}
+	if lists.children[1].isValue != false && lists.children[1].children[0].value == 2 {
+		t.Error(lists.children[1])
+	}
 }
 
 func Test_part1(t *testing.T) {
 	// Arrange
 	input := io.ReadData("13_test")
 	groups := strings.Split(input, "\r\n\r\n")
-	for _, group := range groups {
+
+	// Act
+	sum := 0
+	for i, group := range groups {
 		parts := strings.Split(group, "\r\n")
 		first, _ := createElement(parts[0])
 		second, _ := createElement(parts[1])
-		fmt.Print(first, second)
+		compare := first.compare(second)
+		if compare == 1 {
+			sum += i + 1
+		}
 	}
 
-	// Act
-
 	// Assert
-
+	if diff := cmp.Diff(sum, 13); diff != "" {
+		t.Error(diff)
+	}
 }
