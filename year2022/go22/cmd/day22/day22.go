4e2423fa4d423dac28e7cd2fd8286dd96c51043d
Soren Schwartz <s_schwartz@live.dk>
2022-12-26 21:32:41 +0100

    2022 day22 part2
    Done with 2022 🥳

--- year2022/go22/cmd/day22/day22.go
+++ year2022/go22/cmd/day22/day22.go
@@ -15,11 +15,89 @@ func main() {
 
 	password := jungleMap.followInstructions(instructions, yMax, xMax)
 
-	fmt.Printf("Part 1: %d", password)
+	fmt.Printf("Part 1: %d\n", password)
+
+	password = jungleMap.followCubeInstructions(instructions, yMax, xMax)
+
+	fmt.Printf("Part 2: %d\n", password)
 }
 
 type coord struct {
 	x, y int
+}
+
+func (c coord) addCube(direction coord, xMax, yMax int) (coord, coord) {
+	next := coord{c.x + direction.x, c.y + direction.y}
+	// A1
+	if 0 <= c.x && c.x <= 49 && c.y == 100 && next.y == 99 {
+		return direction.rotateRight(), coord{50, c.x%50 + 50}
+	}
+	// A2
+	if 50 <= c.y && c.y <= 99 && c.x == 50 && next.x == 49 {
+		return direction.rotateLeft(), coord{c.y % 50, 100}
+	}
+	// B1
+	if 100 <= c.y && c.y <= 149 && c.x == 0 && next.x == -1 {
+		return direction.rotateRight().rotateRight(),
+			coord{50, 49 - c.y%50}
+	}
+	// B2
+	if 0 <= c.y && c.y <= 49 && c.x == 50 && next.x == 49 {
+		return direction.rotateRight().rotateRight(),
+			coord{0, 149 - c.y%50}
+	}
+	// C1
+	if 150 <= c.y && c.y <= 199 && c.x == 49 && next.x == 50 {
+		return direction.rotateLeft(),
+			coord{c.y%50 + 50, 149}
+	}
+	// C2
+	if 50 <= c.x && c.x <= 99 && c.y == 149 && next.y == 150 {
+		return direction.rotateRight(),
+			coord{49, 150 + c.x%50}
+	}
+	// D1
+	if 150 <= c.y && c.y <= 199 && c.x == 0 && next.x == -1 {
+		return direction.rotateLeft(),
+			coord{c.y%50 + 50, 0}
+	}
+	// D2
+	if 50 <= c.x && c.x <= 99 && c.y == 0 && next.y == -1 {
+		return direction.rotateRight(),
+			coord{0, 150 + c.x%50}
+	}
+	// E1
+	if 100 <= c.y && c.y <= 149 && c.x == 99 && next.x == 100 {
+		return direction.rotateLeft().rotateLeft(),
+			coord{149, 49 - c.y%50}
+	}
+	// E2
+	if 0 <= c.y && c.y <= 49 && c.x == 149 && next.x == 150 {
+		return direction.rotateLeft().rotateLeft(),
+			coord{99, 149 - c.y%50}
+	}
+	// F1
+	if 100 <= c.x && c.x <= 149 && c.y == 49 && next.y == 50 {
+		return direction.rotateRight(),
+			coord{99, c.x%50 + 50}
+	}
+	// F2
+	if 50 <= c.y && c.y <= 99 && c.x == 99 && next.x == 100 {
+		return direction.rotateLeft(),
+			coord{100 + c.y%50, 49}
+	}
+	// G1
+	if 0 <= c.x && c.x <= 49 && c.y == 199 && next.y == 200 {
+		return direction,
+			coord{100 + c.x%50, 0}
+	}
+	// G2
+	if 100 <= c.x && c.x <= 149 && c.y == 0 && next.y == -1 {
+		return direction,
+			coord{c.x % 50, 199}
+	}
+
+	return direction, coord{modulo(c.x+direction.x, xMax), modulo(c.y+direction.y, yMax)}
 }
 
 func (c coord) add(other coord, xMax, yMax int) coord {
@@ -64,6 +142,43 @@ func (j jungle) findStart(maxX, maxY int) coord {
 		}
 	}
 	return coord{}
+}
+
+func (j jungle) followCubeInstructions(instructions []string, maxY, maxX int) int {
+	position := j.findStart(maxX, maxY)
+	direction := coord{1, 0}
+	for _, elm := range instructions {
+		if elm == "R" {
+			direction = direction.rotateRight()
+			continue
+		}
+		if elm == "L" {
+			direction = direction.rotateLeft()
+			continue
+		}
+		walk, err := strconv.Atoi(elm)
+		if err != nil {
+			panic(err)
+		}
+		for w := 0; w < walk; w++ {
+			newDirection, newPosition := position.addCube(direction, maxX, maxY)
+			q, ok := j[newPosition]
+			if !ok {
+				panic(newPosition)
+			}
+			// for !ok || q == ' ' {
+			// 	newPosition = newPosition.add(direction, maxX, maxY)
+			// 	q, ok = j[newPosition]
+			// }
+			if q == '#' {
+				break
+			}
+			direction = newDirection
+			position = newPosition
+		}
+	}
+	return 1_000*(position.y+1) + 4*(position.x+1) +
+		direction.getFacing()
 }
 
 func (j jungle) followInstructions(instructions []string, maxY, maxX int) int {
