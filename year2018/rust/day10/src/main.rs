241a240da12e1b2c0bfaab2ddd935b896dd6a463
Soren Schwartz <s_schwartz@live.dk>
2023-01-16 22:39:35 +0100

    2018 day10 made write to file

--- year2018/rust/day10/src/main.rs
+++ year2018/rust/day10/src/main.rs
@@ -1,11 +1,14 @@
-use std::{collections::HashSet, fs};
+use std::{collections::HashSet, fs::{self, File}, io::{Write, self}};
 
 fn main() {
     let input = fs::read_to_string("../data/day10_data.txt")
     .expect("file");
 
     let mut sky = Sky::new(input);
-    sky.find_message();
+    match sky.find_message() {
+        Ok(_) => {},
+        Err(err) => println!("{}", err),
+    };
 }
 
 fn generate_point(input: &str) -> (i32, i32) {
@@ -69,7 +72,7 @@ impl Sky {
         return Self { records, x_min: 0, x_max: 0, y_min: 0, y_max: 0 }
     }
 
-    fn find_message(&mut self) {
+    fn find_message(&mut self) -> Result<(), io::Error> {
         let mut distance: i32 = 0;
         for i in 0..1_000_000 {
             self.update();
@@ -77,12 +80,13 @@ impl Sky {
             let y_abs = (self.y_max - self.y_min).abs();
             if distance < x_abs + y_abs && i != 0 {
                 self.rollback();
-                self.print();
+                self.print()?;
                 println!("After: {}", i);
                 break;
             }
             distance = x_abs + y_abs;
         }
+        return Ok(());
     }
 
     fn update(&mut self) {
@@ -112,24 +116,29 @@ impl Sky {
         }
     }
 
-    fn print(&self) {
+    fn print(&self) -> Result<(), io::Error>{
         let sky : &HashSet<Coord>= &self.records
             .iter()
             .map(|r| Coord::new(r))
             .collect();
+        let mut file = File::create("result.txt")?;
         for y in self.y_min..=self.y_max {
             for x in self.x_min..=self.x_max {
                 if sky.contains(&Coord::new_simple(x, y)) {
                     print!("#");
+                    file.write(b"#")?;
                     continue;
                 }
+                file.write(b".")?;
                 print!(".");
             }
+            file.write(b"\n")?;
             println!()
         }
         println!();
         println!("---------------------------------");
         println!();
+        return Ok(());
     }
 }
 
@@ -160,6 +169,9 @@ mod test {
             .expect("file");
             
         let mut sky = Sky::new(input);
-        sky.find_message();
+        match sky.find_message() {
+            Ok(_) => {},
+            Err(err) => println!("{}", err),
+        };
     }
 }
