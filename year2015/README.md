cda0a61b7d7a3a6c1fc881da6efd1310dbcae6ff
schwartz <>
2023-04-23 19:55:20 +0200

    Refactor to library

---
+++ year2015/README.md
@@ -0,0 +1,12 @@
+# 2015 Benchmarks
+
+* https://doc.rust-lang.org/cargo/commands/cargo-bench.html
+* https://crates.io/crates/criterion
+* https://bheisler.github.io/criterion.rs/book/getting_started.html
+* https://bheisler.github.io/criterion.rs/book/faq.html
+* https://bheisler.github.io/criterion.rs/book/user_guide/command_line_output.html
+
+## Day 7
+
+### Rust
+
--- year2015/rust/day7/src/lib.rs
+++ year2015/rust/day7/src/lib.rs
@@ -1,8 +1,10 @@
 use core::fmt;
-use std::{error::Error, fs, collections::HashMap, str::Lines};
+use std::{error::Error, collections::HashMap, str::Lines};
+
+pub type AoCResult<T> = Result<T, AoCError>;
 
 #[derive(Debug)]
-enum AoCError {
+pub enum AoCError {
     LineNotCorrectLength(String),
     MissingKey(String),
     UnknownSignal(String),
@@ -22,30 +24,7 @@ impl fmt::Display for AoCError {
 
 impl Error for AoCError{}
 
-type AoCResult<T> = Result<T, AoCError>;
-
-
-fn main() -> AoCResult<()> {
-    let input = fs::read_to_string("../data/day7_data.txt").unwrap();
-    let lines = input.lines();
-    let mut signals: HashMap<&str, u16> = HashMap::new();
-
-    find_signal(&mut signals, &lines)?;
-
-    let a = signals.get("a").ok_or(AoCError::NoResult)?;
-    println!("Part 1: {}", a);
-
-    let mut signals_part2: HashMap<&str, u16> = HashMap::from([("b", *a)]);
-    
-    find_signal(&mut signals_part2, &lines)?;
-    let a_part2 = signals_part2.get("a").ok_or(AoCError::NoResult)?;
-    println!("Part 2: {}", a_part2);
-
-    Ok(())
-}
-
-
-fn find_signal<'a>(signals: &mut HashMap<&'a str, u16>, lines: &'a Lines) -> AoCResult<()> {
+pub fn find_signal<'a>(signals: &mut HashMap<&'a str, u16>, lines: &'a Lines) -> AoCResult<()> {
     let mut count = 0;
 
     while !signals.contains_key("a") {
