e441fbfca883414dc940799c0cd3e7f1019434a4
schwartz <>
2023-05-12 21:43:58 +0200

    2015 day16 part2

--- year2015/rust/day16/src/main.rs
+++ year2015/rust/day16/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::HashSet};
+use std::{fs, collections::HashMap};
 
 use anyhow::{Result, anyhow};
 
@@ -16,17 +16,17 @@ fn get_type_count(part: &str) -> Result<(&str, i32)> {
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day16_data.txt")?;
 
-    let mut sue = HashSet::new();
-    sue.insert(("children", 3));
-    sue.insert(("cats", 7));
-    sue.insert(("samoyeds", 2));
-    sue.insert(("pomeranians", 3));
-    sue.insert(("akitas", 0));
-    sue.insert(("vizslas", 0));
-    sue.insert(("goldfish", 5));
-    sue.insert(("trees", 3));
-    sue.insert(("cars", 2));
-    sue.insert(("perfumes", 1));
+    let mut sue = HashMap::new();
+    sue.insert("children", 3);
+    sue.insert("cats", 7);
+    sue.insert("samoyeds", 2);
+    sue.insert("pomeranians", 3);
+    sue.insert("akitas", 0);
+    sue.insert("vizslas", 0);
+    sue.insert("goldfish", 5);
+    sue.insert("trees", 3);
+    sue.insert("cars", 2);
+    sue.insert("perfumes", 1);
 
     for line in input.lines() {
         let temp: String = line
@@ -40,12 +40,26 @@ fn main() -> Result<()> {
             .map(|s| s.trim_end_matches(","))
             .collect();
 
-        let is_sue = parts.iter()
-            .filter_map(|c| get_type_count(c).ok())
-            .all(|c| sue.contains(&c));
+        let mut part_1 = true;
+        let mut part_2 = true;
+        for part in parts {
+            let (sue_type, sue_count) = get_type_count(part)?;
+            let count = *sue.get(sue_type).ok_or(anyhow!("missing"))?;
 
-        if is_sue {
-            println!("{}", line);
+            part_1 &= count == sue_count;
+
+            part_2 &= match sue_type {
+                "cats" | "trees" => sue_count > count,
+                "pomeranians" | "goldfish" => sue_count < count,
+                _ => count == sue_count
+            };
+        }
+        
+        if part_1 {
+            println!("Part 1: {}", line);
+        }
+        if part_2 {
+            println!("Part 2: {}", line);
         }
     }
 
