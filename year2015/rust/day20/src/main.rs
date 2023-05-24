df56c995e3adc7e5f16d9ac270c20bd1eb849d6f
schwartz <>
2023-05-24 08:36:24 +0200

    2015 day20 refactor

--- year2015/rust/day20/src/main.rs
+++ year2015/rust/day20/src/main.rs
@@ -1,27 +1,43 @@
+use anyhow::{Result, anyhow, Ok};
 
-
-fn main() {
+fn main() -> Result<()>{
     let input = 29000000;
 
-    let lowest_house = bs(input);
+    let lowest_house = force(input, input, |house| find_divisors(house, 10))
+        .ok_or_else(|| anyhow!("not able to find part 1"))?;
     println!("Part 1: {}", lowest_house);
 
-    let lowest_house = force(input, 10);
-    println!("Part 1: {}", lowest_house);
-
-    let lowest_house = force2(input, 11);
+    let lowest_house = force(input, 50, |house| find_divisors2(house, 11))
+        .ok_or_else(|| anyhow!("not able to find part 2"))?;
     println!("Part 2: {}", lowest_house);
+    
+    Ok(())
 }
 
-fn force2(find: u64, multiple: u64) -> u64 {
-    let base = find / 500;
-    for i in base..=find {
-        let present = find_divisors2(i, multiple);
+fn force(find: u64, base: u64, find_div: fn(u64) -> u64) -> Option<u64> {
+    for i in (find / base)..=find {
+        let present = find_div(i);
         if present > find {
-            return i
+            return Some(i)
         }
     }
-    0
+    None
+}
+
+fn find_divisors(house: u64, multiple: u64) -> u64 {
+    let mut presents = 0;
+    let sqrt_n = (house as f64).sqrt() as u64;
+    for i in 1..=sqrt_n {
+        if house % i == 0 {
+            presents += i * multiple;
+
+            let quotient = house / i;
+            if quotient != i {
+                presents += quotient * multiple;
+            }
+        }
+    }
+    presents
 }
 
 fn find_divisors2(house: u64, multiple: u64) -> u64 {
@@ -43,16 +59,8 @@ fn find_divisors2(house: u64, multiple: u64) -> u64 {
     presents
 }
 
-fn force(find: u64, multiple: u64) -> u64 {
-    for i in 1..find {
-        let present = find_divisors(i, multiple);
-        if present > find {
-            return i
-        }
-    }
-    0
-}
 
+/// Not working since results are not sorted.
 #[allow(dead_code)]
 fn bs(find: u64) -> u64 {
     let mut low = 0;
@@ -70,20 +78,4 @@ fn bs(find: u64) -> u64 {
         }
     }
     low
-}
-
-fn find_divisors(house: u64, multiple: u64) -> u64 {
-    let mut presents = 0;
-    let sqrt_n = (house as f64).sqrt() as u64;
-    for i in 1..=sqrt_n {
-        if house % i == 0 {
-            presents += i * multiple;
-
-            let quotient = house / i;
-            if quotient != i {
-                presents += quotient * multiple;
-            }
-        }
-    }
-    presents
 }
