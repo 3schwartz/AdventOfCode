0978c3a7edfe126524aace6e1289bab7612aafe5
schwartz <>
2023-06-03 14:11:37 +0200

    2015 day24

--- year2015/rust/day24/src/main.rs
+++ year2015/rust/day24/src/main.rs
@@ -1,94 +1,101 @@
-use std::{collections::BTreeSet, fs};
+use std::fs;
 
 use anyhow::Result;
+use itertools::Itertools;
 
-fn dfs(
-    states: [BTreeSet<u32>; 3],
-    mut missing: BTreeSet<u32>,
-    split: u32,
-    smallest: u32,
-    visited: &mut BTreeSet<(BTreeSet<BTreeSet<u32>>, BTreeSet<u32>)>,
-) -> u32 {
-    let compare = BTreeSet::from(states.clone());
-    if !visited.insert((compare, missing.clone())) {
-        return u32::MAX;
+struct Groups {
+    presents: Vec<u64>,
+    split: u64,
+    size: u64,
+}
+
+impl Groups {
+    fn new(presents: &Vec<u64>, size: u64) -> Self {
+        let split = presents.iter().sum::<u64>() / size;
+        Self { presents: presents.clone(), split, size }
     }
 
-    let Some(next) = missing.pop_first() else { return u32::MAX; };
+    fn from(used: Vec<u64>, groups: &Groups) -> Self {
+        let presents: Vec<u64> = groups.presents
+            .iter()
+            .filter(|v| !used.contains(v))
+            .map(|v| *v)
+            .collect();
+        Self { presents, split: groups.split, size: groups.size - 1}
+    }
 
-    let mut smallest_current = smallest;
-
-    let first_sum = states[0].iter().sum::<u32>();
-    let second_sum = states[1].iter().sum::<u32>();
-    let third_sum = states[2].iter().sum::<u32>();
-
-    if first_sum == split && first_sum == second_sum && second_sum == third_sum {
-        let mut min_len = states[0].len();
-        let mut min_idx = 0;
-        for i in 0..3 {
-            if states[i].len() < min_len {
-                min_idx = i;
-                min_len = states[i].len();
+    fn can_group(groups: Groups) -> bool {
+        if groups.size == 0 {
+            return true;
+        }
+        if groups.size == 1 {
+            let ok = groups.presents.iter().sum::<u64>() == groups.split;
+            return ok;
+        }
+    
+        for group in &groups {
+            let new_groups = Groups::from(group, &groups);
+            if Groups::can_group(new_groups) {
+                return true;
             }
         }
-        return states[min_idx].iter().fold(1, |acc, &num| acc * num);
+    
+        return false;
     }
 
-    if first_sum + next > split && first_sum != split
-        || second_sum + next > split && second_sum != split
-        || third_sum + next > split && third_sum != split
-    {
-        return smallest_current;
-    }
+    fn find_qe(&self) -> u64 {
+        let mut min_qe = u64::MAX;
 
-    println!("{}, {}, {}, {}", first_sum, second_sum, third_sum, next);
-
-    if first_sum + next < split {
-        let mut cloned = states.clone();
-        cloned[0].insert(next);
-        let qe = dfs(cloned, missing.clone(), split, smallest_current, visited);
-        if qe < smallest_current {
-            smallest_current = qe;
+        for group in self {
+    
+            let qe = group.iter().fold(1, |acc, n| acc * n);
+            if qe >= min_qe {
+                continue;
+            }
+    
+            let new_groups = Groups::from(group, self);
+            let is_able_to_group = Groups::can_group(new_groups);
+    
+            if !is_able_to_group {
+                break;
+            }
+    
+            min_qe = qe;
+            break;
         }
+        min_qe
     }
+}
 
-    if second_sum + next < split {
-        let mut cloned = states.clone();
-        cloned[1].insert(next);
-        let qe = dfs(cloned, missing.clone(), split, smallest_current, visited);
-        if qe < smallest_current {
-            smallest_current = qe;
-        }
-    }
-    if third_sum + next < split {
-        let mut cloned = states.clone();
-        cloned[2].insert(next);
-        let qe = dfs(cloned, missing.clone(), split, smallest_current, visited);
-        if qe < smallest_current {
-            smallest_current = qe;
-        }
-    }
 
-    return smallest_current;
+impl Iterator for &Groups {
+    type Item = Vec<u64>;
+
+    fn next(&mut self) -> Option<Vec<u64>> {
+        for l in 0..self.presents.len() {
+            for group in self.presents.iter().map(|v| *v).combinations(l) {
+                if group.iter().sum::<u64>() == self.split {
+                    return Some(group);
+                }
+            }
+        }
+        return None
+    }
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day24_data.txt")?;
+    let presents: Vec<u64> = input.lines().flat_map(|l| l.parse()).collect();
 
-    let presents: BTreeSet<u32> = input.lines().flat_map(|l| l.parse()).collect();
+    let groups = Groups::new(&presents, 3);
+    let min_qe = groups.find_qe();
 
-    let sum = presents.iter().sum::<u32>();
-    let value: u32 = sum / 3;
+    println!("Part 1: {}", min_qe);
 
-    let qe = dfs(
-        [BTreeSet::new(), BTreeSet::new(), BTreeSet::new()],
-        presents,
-        value,
-        u32::MAX,
-        &mut BTreeSet::new(),
-    );
+    let groups = Groups::new(&presents, 4);
+    let min_qe = groups.find_qe();
 
-    println!("Part 1: {}", qe);
+    println!("Part 2: {}", min_qe);
 
     Ok(())
 }
