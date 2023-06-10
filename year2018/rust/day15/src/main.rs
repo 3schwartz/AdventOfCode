9050e111e5ea20acb304ff45608de13695952568
schwartz <s_schwartz@live.dk>
2023-06-10 20:57:17 +0200

    2018 day15 part1 test

--- year2018/rust/day15/src/main.rs
+++ year2018/rust/day15/src/main.rs
@@ -6,27 +6,27 @@ use std::{
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day15_data.txt")?;
+    let mut game = Game::from(input)?;
 
-    for line in input.lines() {
-        println!("{}", line);
-    }
+    let part_1 = game.play()?;
 
+    println!("Part 1: {}", part_1);
     Ok(())
 }
 
 struct Visits {
     first_move: (usize, usize),
     location: (usize, usize),
-    elem: Elem,
+    // elem: Elem,
     steps: u32,
 }
 
 impl Visits {
-    fn new(elem: Elem, first_move: (usize, usize)) -> Self {
+    fn new(first_move: (usize, usize)) -> Self {
         Self {
             first_move,
             location: first_move.clone(),
-            elem,
+            // elem,
             steps: 1,
         }
     }
@@ -35,7 +35,7 @@ impl Visits {
         Visits {
             first_move: self.first_move,
             location: new_location,
-            elem: self.elem.clone(),
+            // elem: self.elem.clone(),
             steps: self.steps + 1,
         }
     }
@@ -53,35 +53,75 @@ impl PartialEq for Elem {
         match (self, other) {
             (Self::Elf(_), Self::Elf(_)) => true,
             (Self::Goblin(_), Self::Goblin(_)) => true,
+            (Self::Empty, Self::Empty) => true,
             _ => false,
         }
     }
 }
 
 impl Elem {
+    fn damage(&self, damage: u8) -> Elem {
+        let mut cloned = self.clone();
+        match cloned {
+            Elem::Elf(ref mut life) | Elem::Goblin(ref mut life) => {
+                *life = life.saturating_sub(damage);
+            }
+            Elem::Empty => (),
+        };
+        cloned
+    }
 
-    fn find_opponent<'a>(&self, location: &(usize, usize), map: &'a HashMap<(usize, usize), Elem>) -> Option<&'a Elem> {
+    fn find_opponent<'a>(
+        &self,
+        location: &(usize, usize),
+        map: &'a HashMap<(usize, usize), Elem>,
+    ) -> Option<(&'a Elem, (usize, usize))> {
+        if self == &Elem::Empty {
+            return None;
+        }
+
         let neighbors = Elem::get_neigbors(&location);
-        let mut possible: Option<&Elem> = None;
+        let mut possible: Option<(&Elem, (usize, usize))> = None;
         for neighbor in &neighbors {
             let Some(n) = map.get(neighbor) else { continue; };
             if !self.is_opposite(n) {
                 continue;
             }
 
-            let Some(other) = possible else {
-                possible = Some(n);
+            let Some((possible_elem, possible_location)) = possible else {
+                possible = Some((n, *neighbor));
                 continue;
             };
 
-            match possible {
-                Some(other) => {
-                    // if other
-                },
-                None => possible = Some(n),
+            let Some(possible_life) = possible_elem.get_life() else {continue;};
+            let Some(neighbor_life) = n.get_life() else {continue;};
+
+            if possible_life < neighbor_life {
+                continue;
             }
-        };
+            if neighbor_life < possible_life {
+                possible = Some((n, *neighbor));
+                continue;
+            }
+            if possible_location.1 < neighbor.1 {
+                continue;
+            }
+            if neighbor.1 < possible_location.1 {
+                possible = Some((n, *neighbor));
+                continue;
+            }
+            if neighbor.0 < possible_location.0 {
+                possible = Some((n, *neighbor));
+            }
+        }
         possible
+    }
+
+    fn get_life(&self) -> Option<&u8> {
+        match self {
+            Elem::Elf(life) | Elem::Goblin(life) => Some(life),
+            Elem::Empty => None,
+        }
     }
 
     fn find_next_location(
@@ -89,7 +129,6 @@ impl Elem {
         location: &(usize, usize),
         map: &HashMap<(usize, usize), Elem>,
     ) -> Result<(usize, usize)> {
-
         let neighbors = Elem::get_neigbors(location);
 
         for neighbor in &neighbors {
@@ -103,7 +142,7 @@ impl Elem {
         let mut visited = HashSet::new();
         let mut queue = neighbors
             .into_iter()
-            .map(|n| Visits::new(self.clone(), n))
+            .map(|n| Visits::new(n))
             .collect::<VecDeque<Visits>>();
         let mut reachable = vec![];
         let mut min_steps = u32::MAX;
@@ -157,12 +196,12 @@ impl Elem {
         match (self, other) {
             (Elem::Elf(_), Elem::Goblin(_)) => true,
             (Elem::Goblin(_), Elem::Elf(_)) => true,
-            _ => false
+            _ => false,
         }
     }
 
     fn get_neigbors(location: &(usize, usize)) -> Vec<(usize, usize)> {
-        let mut neigh = vec![(location.0, location.1), (location.0, location.1)];
+        let mut neigh = vec![(location.0 + 1, location.1), (location.0, location.1 + 1)];
         if location.0 != 0 {
             neigh.push((location.0 - 1, location.1))
         }
@@ -180,7 +219,6 @@ struct Game {
 }
 
 impl Game {
-
     fn from(input: String) -> Result<Game> {
         let mut map: HashMap<(usize, usize), Elem> = HashMap::new();
 
@@ -218,12 +256,11 @@ impl Game {
                     let next_location = elem.find_next_location(location, &self.map)?;
                     moved_map.insert(next_location, elem.clone());
                 }
-                Elem::Empty => {
-                    if !moved_map.contains_key(location) {
-                        moved_map.insert(*location, Elem::Empty);
-                    };
-                }
-            }
+                Elem::Empty => (),
+            };
+            if !moved_map.contains_key(location) {
+                moved_map.insert(*location, Elem::Empty);
+            };
         }
 
         self.map = moved_map;
@@ -231,58 +268,93 @@ impl Game {
     }
 
     fn attack_with_elements(&mut self) -> Result<()> {
-
         for x in 0..=self.x_max {
             for y in 0..=self.y_max {
-                let location = (x,y);
+                let location = (x, y);
                 let Some(next) = self.map.get(&location) else {continue;};
-
-                let neighbors = Elem::get_neigbors(&location);
-                // for neighbor in 
+                let Some((opponent, opponent_location)) = next.find_opponent(&location, &self.map) else {continue;};
+                let opppnent_damaged = opponent.damage(3);
+                self.map.insert(opponent_location, opppnent_damaged);
             }
+        }
+
+        for (_, v) in &mut self.map {
+            match *v {
+                Elem::Elf(life) | Elem::Goblin(life) => {
+                    if life > 0 {
+                        continue;
+                    };
+                    *v = Elem::Empty;
+                }
+                Elem::Empty => (),
+            };
         }
 
         Ok(())
     }
+
+    fn all_elves_dead(&self) -> bool {
+        for (_, v) in &self.map {
+            match v {
+                Elem::Elf(_) => return false,
+                _ => (),
+            }
+        }
+        return true;
+    }
+
+    /// Round
+    /// For each
+    ///     if none in range then move
+    ///         find in range & reachable
+    ///         find nearest
+    ///         chose best using reading order
+    ///     attack
+    ///         find and in range
+    ///         smallest hit point
+    ///         if tie then reading order
+    ///         if dead then out
+    ///
+    /// Find number of full rounds completed before none elf
+    fn play(&mut self) -> Result<u32> {
+        let mut round = 1;
+        loop {
+            self.move_elemements()?;
+            self.attack_with_elements()?;
+
+            if self.all_elves_dead() {
+                break;
+            }
+            round += 1;
+        }
+        let hit_point_sum: u32 = self
+            .map
+            .values()
+            .map(|v| match v {
+                Elem::Goblin(life) => *life as u32,
+                _ => 0,
+            })
+            .sum::<u32>();
+        let outcome = round * hit_point_sum;
+        Ok(outcome)
+    }
 }
-
-/// Round
-/// For each
-///     if none in range then move
-///         find in range & reachable
-///         find nearest
-///         chose best using reading order
-///     attack
-///         find and in range
-///         smallest hit point
-///         if tie then reading order
-///         if dead then out
-///
-/// Find number of full rounds completed before none elf
-///     
-///
-fn round() -> () {}
-// Round
-// For each
-//  find in range
-// find
-
 
 #[cfg(test)]
 mod test {
+    use anyhow::Ok;
 
-    enum Foo {
-        Bar(u16)
-    }
+    use super::*;
 
     #[test]
-    fn test_enum() -> (){
-        let b = Foo::Bar(1);
-        let a = Foo::Bar(2);
+    fn test_part_1() -> Result<()> {
+        let expected = 27730;
+        let input = fs::read_to_string("../../data/day15_test_data.txt")?;
+        let mut game = Game::from(input)?;
 
-        match b {
-            Foo::Bar(a) => todo!(),
-        }
+        let part_1 = game.play()?;
 
+        assert_eq!(expected, part_1);
+        Ok(())
     }
 }
