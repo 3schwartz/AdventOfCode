b21b0987007abb6503d68d73b007dece78e00180
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-07-01 09:28:29 +0200

    2018 day19 refactor

--- year2018/rust/day19/src/main.rs
+++ year2018/rust/day19/src/main.rs
@@ -1,92 +1,129 @@
-use std::{collections::{HashMap, BTreeMap}, fs};
+use std::{
+    collections::{BTreeMap, HashMap},
+    fs,
+};
 
 use anyhow::{anyhow, Result};
 
-fn sum_of_divisors(n: u32) -> u32 {
-    let mut sum = 0;
-    let mut j = 1;
-    loop {
-        if j * j > n {
-            break;
-        }
-        if (n % j) == 0 {
-            if n / j == j {
-                sum += j;
-            }
-            else {
-                sum += j + n / j;
-            }
-        }
-        j+=1;
-    }
-    
-    return sum;
-}
-
 fn main() -> Result<()> {
-    let div = sum_of_divisors(10551326);
-
-    println!("{}", div);
     let input = fs::read_to_string("../data/day19_data.txt")?;
+    let program = Program::from(&input)?;
 
-    let possible_bound = input.lines().next();
-    let bound = Instruction::get_bound(possible_bound)?;
-    let mut instructions = HashMap::new();
-    for (idx, line) in input.lines().skip(1).enumerate() {
-        let instruction = Instruction::from(line)?;
-        instructions.insert(idx as u32, instruction);
-    }
-
-
-    // let mut register = HashMap::new();
-    let debug = true;
-    let mut register = BTreeMap::from([(0, 1)]);
-    let mut pointer = 0;
-    loop {
-        let Some(instruction) = instructions.get(&pointer) else {
-            break;
-        };
-        if debug {
-            
-            println!("{}", pointer);
-            println!("{:?}", instruction);
-            println!("{:?}", register);   
-        }
-        if pointer == 3 {
-            let reg_2 = *register.get(&2).ok_or_else(|| anyhow!("should have 2"))?;
-            let reg_4 = *register.get(&4).ok_or_else(|| anyhow!("should have 4"))?;
-            if reg_4 % reg_2 == 0 {
-                let reg_0 = register.get(&0).ok_or_else(|| anyhow!("should have 0"))?;
-                register.insert(0, reg_0 + reg_2);
-            };
-            register.insert(1, 1);
-            register.insert(5, reg_4);
-            pointer = 12;
-            continue;
-        }
-
-        register.insert(bound, pointer);
-
-        instruction.opcode.invoke(
-            instruction.input_a,
-            instruction.input_b,
-            instruction.output,
-            &mut register,
-        )?;
-
-        pointer = *register
-            .get(&bound)
-            .ok_or_else(|| anyhow!("should exists: {:?}", register))?;
-
-        pointer += 1;
-    }
-    let part_1 = register
-        .get(&0)
-        .ok_or_else(|| anyhow!("registry missing 0: {:?}", register))?;
-
+    let part_1 = program.run(BTreeMap::new(), false)?;
     println!("Part 1: {}", part_1);
 
+    let part_2 = program.run(BTreeMap::from([(0, 1)]), false)?;
+    println!("Part 2: {}", part_2);
+
+    let div = Program::sum_of_divisors(10551326);
+    println!("Part 2 divisors: {}", div);
+
     Ok(())
+}
+
+struct Program {
+    bound: u32,
+    instructions: HashMap<u32, Instruction>,
+}
+
+impl Program {
+    fn from(input: &str) -> Result<Self> {
+        let possible_bound = input.lines().next();
+        let bound = Instruction::get_bound(possible_bound)?;
+        let mut instructions = HashMap::new();
+        for (idx, line) in input.lines().skip(1).enumerate() {
+            let instruction = Instruction::from(line)?;
+            instructions.insert(idx as u32, instruction);
+        }
+        Ok(Self {
+            bound,
+            instructions,
+        })
+    }
+
+    /// Part 2 is sum of divisors for a numbers. The high numbers
+    /// is in registry 4 when the loops start.
+    fn sum_of_divisors(n: u32) -> u32 {
+        let mut sum = 0;
+        let mut j = 1;
+        loop {
+            if j * j > n {
+                break;
+            }
+            if (n % j) == 0 {
+                if n / j == j {
+                    sum += j;
+                } else {
+                    sum += j + n / j;
+                }
+            }
+            j += 1;
+        }
+
+        return sum;
+    }
+
+    fn run(&self, mut register: BTreeMap<u32, u32>, debug: bool) -> Result<u32> {
+        let mut pointer = 0;
+        // Used to differentiate between part 1 and part 2.
+        let initial_0 = *register.get(&0).unwrap_or(&0);
+        loop {
+            let Some(instruction) = self.instructions.get(&pointer) else {
+                break;
+            };
+            if debug {
+                println!("{}", pointer);
+                println!("{:?}", instruction);
+                println!("{:?}", register);
+            }
+            // Relevant for part 2 which is problem of getting all divisors of a high number.
+            // The operations are a outer and a inner loop.
+            // The outer loop starts with reg 2 = 1 and increases reg 2 all the way up to reg 4 (the high number).
+            // The outer loop goes from (3) -> inner loop -> 12, 13, 14, 15.
+            // The inner loop validates if the current value of reg 2 is a divisor of reg 4 and adds reg 2 (the divisor)
+            // to reg 0 if it is a dvisior.
+            if pointer == 3 && initial_0 != 0 {
+                let reg_2 = *register.get(&2).ok_or_else(|| anyhow!("should have 2"))?;
+                let reg_4 = *register.get(&4).ok_or_else(|| anyhow!("should have 4"))?;
+                // First, instructions 3, 4, 5, 6, 8, 9, 10, 11 is run in a loop. This is the inner loop.
+                // This one essentially increases registry 5 from 1 to registry 4 (the high number) by one for each iteration
+                // and in each iteration checks if reg 2 * reg 5 = reg 4.
+                // This can be validated by checking if reg 2 is a divisor of reg 4 => reg 4 % reg 2 == 0.
+                if reg_4 % reg_2 == 0 {
+                    // If it is a divisor reg 2 (the divisor) is added to reg 0 - hence the sum of divisors.
+                    let reg_0 = register.get(&0).ok_or_else(|| anyhow!("should have 0"))?;
+                    register.insert(0, reg_0 + reg_2);
+                };
+                // Is overwritten in outer loop.
+                register.insert(1, 1);
+                // The inner loop goes from 1 to reg 4 in reg 5. Hence last step of inner loop is having these equal.
+                register.insert(5, reg_4);
+                // When reg 4 == reg 5 then one leaves the inner loop.
+                pointer = 12;
+                // The outer loop now increases reg 2, and the outer loop is broken when reg 2 > reg 4.
+                continue;
+            }
+
+            register.insert(self.bound, pointer);
+
+            instruction.opcode.invoke(
+                instruction.input_a,
+                instruction.input_b,
+                instruction.output,
+                &mut register,
+            )?;
+
+            pointer = *register
+                .get(&self.bound)
+                .ok_or_else(|| anyhow!("should exists: {:?}", register))?;
+
+            pointer += 1;
+        }
+        let reg_0 = register
+            .get(&0)
+            .ok_or_else(|| anyhow!("registry missing 0: {:?}", register))?;
+        Ok(*reg_0)
+    }
 }
 
 #[derive(Debug)]
