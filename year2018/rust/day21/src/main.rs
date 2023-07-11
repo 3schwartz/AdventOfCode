58c5f3b296cca9e420b2cbba42f219083f2b4be7
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-07-11 13:28:25 +0200

    2018 day21 refactor and docs

--- year2018/rust/day21/src/main.rs
+++ year2018/rust/day21/src/main.rs
@@ -12,17 +12,26 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn run(program: &Program, register: &mut BTreeMap<u128, u128>) -> Result<u32> {
+/// Looking at the input program only instruction 28, eqrr 5 0 2, uses registry 0
+/// which is the only one which can be changed.
+/// When registry 0 is equal to 5 then the program halts.
+/// 
+/// The shortest program, part 1, hence needs to look at the value of registry 5 the first time this instruction is run.
+/// 
+/// The longest program, part 2, needs to look at when a value for registry 5 is seen twice. Then
+/// use the value of registry 5 the last time the instruction was run.
+fn run(program: &Program, register: &mut BTreeMap<u128, u128>) -> Result<()> {
     let mut pointer = 0;
-    let mut count = 0;
+
+    let mut seen_first = false;
+
     let mut last = 0;
     let mut seen = HashSet::new();
     loop {
         let Some(instruction) = program.instructions.get(&pointer) else {
             break;
         };
-        
-        count += 1;
+    
 
         register.insert(program.bound, pointer);
 
@@ -39,11 +48,14 @@ fn run(program: &Program, register: &mut BTreeMap<u128, u128>) -> Result<u32> {
 
         if pointer == 28 {
             let now = *register
-            .get(&5)
-            .unwrap_or(&0);
+                .get(&5)
+                .unwrap_or(&0);
+            if !seen_first {
+                println!("Part 1: {}", now);
+                seen_first = true;
+            }
             if !seen.insert(now) {
-                println!("{}", now);
-                println!("{}", last);
+                println!("Part 2: {}", last);
                 break;
             }
             last = now;
@@ -51,5 +63,5 @@ fn run(program: &Program, register: &mut BTreeMap<u128, u128>) -> Result<u32> {
         
         pointer += 1;
     }
-    Ok(count)
+    Ok(())
 }
