f82eb3373837e998ad54e05ea255c6b3f8a66479
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-07-23 07:25:07 +0200

    2018 day22 refactor to storage

--- year2018/rust/day22/src/main.rs
+++ year2018/rust/day22/src/main.rs
@@ -83,6 +83,16 @@ impl Type {
         Ok(result)
     }
 
+    fn from_coord(
+        x: usize,
+        y: usize,
+        erosion_levels: &mut ErosionLevelStorage,
+        geologic_index: &mut GeologicIndexStorage,
+    ) -> Result<Type> {
+        let erosion_level = erosion_levels.get(x, y, geologic_index);
+        Type::from(erosion_level)
+    }
+
     fn get_risk_level(&self) -> usize {
         match self {
             Type::Rocky => 0,
@@ -121,9 +131,12 @@ struct State {
 }
 
 impl State {
-
     fn new() -> Self {
-        Self { x: 0, y: 0, tool: Tools::Torch }
+        Self {
+            x: 0,
+            y: 0,
+            tool: Tools::Torch,
+        }
     }
 
     fn from_tool(&self, tool: Tools) -> Self {
@@ -164,126 +177,78 @@ impl State {
     }
 }
 
-fn get_type(
-    x: usize,
-    y: usize,
+struct ErosionLevelStorage {
     depth: usize,
-    x_target: usize,
-    y_target: usize,
-    geologic_index: &mut HashMap<(usize, usize), usize>,
-    erosion_levels: &mut HashMap<(usize, usize), usize>,
-) -> Result<Type> {
-    let erosion_level = get_erosion_level_map(
-        x,
-        y,
-        depth,
-        x_target,
-        y_target,
-        geologic_index,
-        erosion_levels,
-    );
-    let region_type = Type::from(erosion_level)?;
-    Ok(region_type)
+    erosion_levels: HashMap<(usize, usize), usize>,
 }
 
-/// Recursive
-fn get_erosion_level_map(
-    x: usize,
-    y: usize,
-    depth: usize,
-    x_target: usize,
-    y_target: usize,
-    geologic_index: &mut HashMap<(usize, usize), usize>,
-    erosion_levels: &mut HashMap<(usize, usize), usize>,
-) -> usize {
-    if let Some(level) = erosion_levels.get(&(x, y)) {
-        return *level;
+impl ErosionLevelStorage {
+    fn new(depth: usize) -> Self {
+        Self {
+            depth,
+            erosion_levels: HashMap::new(),
+        }
     }
 
-    let index = geologic_index.get(&(x, y)).map(|i| *i).unwrap_or_else(|| {
-        get_geologic_index_map(
-            x,
-            y,
-            depth,
+    /// Recursive
+    fn get(&mut self, x: usize, y: usize, geologic_index: &mut GeologicIndexStorage) -> usize {
+        if let Some(level) = self.erosion_levels.get(&(x, y)) {
+            return *level;
+        }
+
+        let index = geologic_index.get(x, y, self);
+
+        let next = (index + self.depth) % 20183;
+
+        self.erosion_levels.insert((x, y), next);
+        next
+    }
+}
+
+struct GeologicIndexStorage {
+    x_target: usize,
+    y_target: usize,
+    geologic_index: HashMap<(usize, usize), usize>,
+}
+
+impl GeologicIndexStorage {
+    fn new(x_target: usize, y_target: usize) -> Self {
+        Self {
             x_target,
             y_target,
-            geologic_index,
-            erosion_levels,
-        )
-    });
-
-    let next = (index + depth) % 20183;
-
-    erosion_levels.insert((x, y), next);
-    next
-}
-
-/// Recursive
-fn get_geologic_index_map(
-    x: usize,
-    y: usize,
-    depth: usize,
-    x_target: usize,
-    y_target: usize,
-    geologic_index: &mut HashMap<(usize, usize), usize>,
-    erosion_levels: &mut HashMap<(usize, usize), usize>,
-) -> usize {
-    if let Some(index) = geologic_index.get(&(x, y)) {
-        return *index;
+            geologic_index: HashMap::new(),
+        }
     }
 
-    let mapped = if x == 0 && y == 0 {
-        0
-    } else if x == x_target && y == y_target {
-        0
-    } else if y == 0 {
-        x * 16807
-    } else if x == 0 {
-        y * 48271
-    } else {
-        erosion_levels
-            .get(&(x, y - 1))
-            .map(|i| *i)
-            .unwrap_or_else(|| {
-                get_erosion_level_map(
-                    x,
-                    y - 1,
-                    depth,
-                    x_target,
-                    y_target,
-                    geologic_index,
-                    erosion_levels,
-                )
-            })
-            * erosion_levels
-                .get(&(x - 1, y))
-                .map(|i| *i)
-                .unwrap_or_else(|| {
-                    get_erosion_level_map(
-                        x - 1,
-                        y,
-                        depth,
-                        x_target,
-                        y_target,
-                        geologic_index,
-                        erosion_levels,
-                    )
-                })
-    };
-    geologic_index.insert((x, y), mapped);
-    return mapped;
+    /// Recursive
+    fn get(&mut self, x: usize, y: usize, erosion_levels: &mut ErosionLevelStorage) -> usize {
+        if let Some(index) = self.geologic_index.get(&(x, y)) {
+            return *index;
+        }
+
+        let mapped = if x == 0 && y == 0 {
+            0
+        } else if x == self.x_target && y == self.y_target {
+            0
+        } else if y == 0 {
+            x * 16807
+        } else if x == 0 {
+            y * 48271
+        } else {
+            erosion_levels.get(x, y - 1, self) * erosion_levels.get(x - 1, y, self)
+        };
+        self.geologic_index.insert((x, y), mapped);
+        return mapped;
+    }
 }
 
 fn get_fastest_route(x_target: usize, y_target: usize, depth: usize) -> Result<usize> {
-    let mut queue = HashMap::from([(
-        0,
-        vec![State::new()],
-    )]);
+    let mut queue = HashMap::from([(0, vec![State::new()])]);
 
     let mut steps = 0;
     let mut visited = HashSet::new();
-    let mut erosion_levels = HashMap::new();
-    let mut geologic_index = HashMap::new();
+    let mut erosion_levels = ErosionLevelStorage::new(depth);
+    let mut geologic_index = GeologicIndexStorage::new(x_target, y_target);
 
     loop {
         if queue.is_empty() {
@@ -302,15 +267,8 @@ fn get_fastest_route(x_target: usize, y_target: usize, depth: usize) -> Result<u
                 return Ok(steps);
             }
 
-            let current_region = get_type(
-                next.x,
-                next.y,
-                depth,
-                x_target,
-                y_target,
-                &mut geologic_index,
-                &mut erosion_levels,
-            )?;
+            let current_region =
+                Type::from_coord(next.x, next.y, &mut erosion_levels, &mut geologic_index)?;
 
             if let Some(other) = next.tool.get_other(current_region) {
                 let next_queue = queue.entry(steps + 7).or_insert_with(|| vec![]);
@@ -320,15 +278,13 @@ fn get_fastest_route(x_target: usize, y_target: usize, depth: usize) -> Result<u
             }
 
             for neighbor in next.get_neighbors() {
-                let neighbor_region = get_type(
+                let neighbor_region = Type::from_coord(
                     neighbor.0,
                     neighbor.1,
-                    depth,
-                    x_target,
-                    y_target,
-                    &mut geologic_index,
                     &mut erosion_levels,
+                    &mut geologic_index,
                 )?;
+
                 if !next.can_go(neighbor_region) {
                     continue;
                 }
