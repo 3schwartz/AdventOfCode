3d6a75c745f791c26767fcd71a60d40c908e1818
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-07-26 08:59:45 +0200

    2016 day1 par1 refactor

--- year2016/haskell/day1/app/Main.hs
+++ year2016/haskell/day1/app/Main.hs
@@ -13,22 +13,17 @@ main = do
 
 
 parseDirection :: ((Int, Int), (Int, Int)) -> String -> ((Int, Int), (Int, Int))
-parseDirection cityGrid input = case splitAt 1 input of
-    ("L", rest) ->
-        let parsed = read rest
-            position = fst cityGrid
-            direction = snd cityGrid
-            newDirection = (-snd direction, fst direction)
-            newPosition = (fst position + fst newDirection * parsed, snd position + snd newDirection * parsed)
-        in (newPosition, newDirection)
-    ("R", rest) ->
-        let parsed = read rest
-            position = fst cityGrid
-            direction = snd cityGrid
-            newDirection = (snd direction, -fst direction)
-            newPosition = (fst position + fst newDirection * parsed, snd position + snd newDirection * parsed)
-        in (newPosition, newDirection)        
-    _ -> ((0,0), (0,0))
+parseDirection cityGrid input = do
+    let (newDirection, parsed) = getMovement (snd cityGrid) input
+    let position = fst cityGrid
+    let newPosition = (fst position + fst newDirection * parsed, snd position + snd newDirection * parsed)
+    (newPosition, newDirection)
+
+getMovement :: (Int, Int) -> String -> ((Int, Int), Int)
+getMovement direction input = case splitAt 1 input of
+    ("L", rest) -> ((-snd direction, fst direction), read rest)
+    ("R", rest) -> ((snd direction, -fst direction), read rest)
+    _ -> ((0,0), 0)    
 
 manhattanDistance :: (Int, Int) -> Int
 manhattanDistance (x, y) = abs x + abs y
