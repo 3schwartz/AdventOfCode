5f5132df5cc6ac89ee3613400871ba3f1f9ccd37
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-07-26 23:28:31 +0200

    2016 day1 part2

--- year2016/haskell/day1/app/Main.hs
+++ year2016/haskell/day1/app/Main.hs
@@ -2,28 +2,84 @@ module Main where
 
 import Data.List (foldl')
 import Data.List.Split (splitOn)
+import qualified Data.Set as S
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
     contents <- readFile "../../data/day1_data.txt"
     let splitted = splitOn ", " contents
-    let result = foldl' parseDirection ((0,0), (0,1)) splitted
-    let distance = manhattanDistance (fst result)
-    print distance
+        initialState = stateNew (coordNew 0 0) (coordNew 0 1)
+
+        part1 = foldl' parseDirection initialState splitted
+        distance = manhattanDistance (position part1)
+    putStrLn $ "Part 1: " ++ show distance
+    
+    let stateWithSet = stateWithSetNew (stateNew (coordNew 0 0) (coordNew 0 1)) (S.empty)
+        part2 = returnIfMultipleVisits stateWithSet splitted
+        distancePart2 = manhattanDistance (position (state part2))
+    putStrLn $ "Part 2: " ++ show distancePart2
+
+data Coord = Coord { x::Int, y::Int }
+    deriving (Show, Eq, Ord)
+coordNew :: Int -> Int -> Coord
+coordNew x y = Coord { x = x, y = y }
+
+data StateWithSet = StateWithSet { state :: State, visited :: S.Set Coord }
+    deriving (Show)
+stateWithSetNew :: State -> S.Set Coord -> StateWithSet
+stateWithSetNew state visited = StateWithSet { state = state, visited = visited }
+
+getState :: StateWithSet -> State
+getState (StateWithSet state _ ) = state
+
+data State = State {position :: Coord, direction :: Coord }
+    deriving (Show)
+
+stateNew :: Coord -> Coord -> State
+stateNew position direction = State { position = position, direction = direction }
+
+returnIfMultipleVisits :: StateWithSet -> [String] -> StateWithSet
+returnIfMultipleVisits stateWithSet [] = stateWithSet
+returnIfMultipleVisits (StateWithSet state visited) (input : inputs)
+    | S.notMember (position state) visited =
+        let updated = S.insert (position state) visited
+            (newDirection, parsed) = getMovement (direction state) input
+            newPosition = Coord { x = x (position state) + x newDirection
+                                , y = y (position state) + y newDirection}
+            (newState, found) = move (stateWithSetNew (stateNew newPosition newDirection) updated) (parsed - 1)
+        in
+            if found
+                then newState
+                else trace ("newState: " ++ show (getState newState) ++ show input) $
+                    returnIfMultipleVisits newState inputs
+    | otherwise = stateWithSetNew state visited
+
+move :: StateWithSet -> Int -> (StateWithSet, Bool)
+move stateWithSet 0 = (stateWithSet, False)
+move (StateWithSet state visited) count
+    | S.member (position state) visited = ((stateWithSetNew state visited), True) 
+    | otherwise =
+        let updated = S.insert (position state) visited
+            newPosition = Coord { x = x (position state) + x (direction state)
+                                , y = y (position state) + y (direction state)}
+            newState = stateWithSetNew (stateNew newPosition (direction state)) updated
+        in trace ("newState: " ++ show (getState newState)) $
+            move newState (count - 1)
 
 
-parseDirection :: ((Int, Int), (Int, Int)) -> String -> ((Int, Int), (Int, Int))
-parseDirection cityGrid input = do
-    let (newDirection, parsed) = getMovement (snd cityGrid) input
-    let position = fst cityGrid
-    let newPosition = (fst position + fst newDirection * parsed, snd position + snd newDirection * parsed)
-    (newPosition, newDirection)
+parseDirection :: State -> String -> State
+parseDirection state input =
+    let (newDirection, parsed) = getMovement (direction state) input
+        newPosition = Coord { x = x (position state) + x newDirection * parsed
+                            , y = y (position state) + y newDirection * parsed}
+    in State { position = newPosition, direction = newDirection }
 
-getMovement :: (Int, Int) -> String -> ((Int, Int), Int)
+getMovement :: Coord -> String -> (Coord, Int)
 getMovement direction input = case splitAt 1 input of
-    ("L", rest) -> ((-snd direction, fst direction), read rest)
-    ("R", rest) -> ((snd direction, -fst direction), read rest)
-    _ -> ((0,0), 0)    
+    ("L", rest) -> (Coord { x = -y direction, y = x direction}, read rest)
+    ("R", rest) -> (Coord { x = y direction, y = -x direction}, read rest)
+    _ -> ((coordNew 0 0), 0)
 
-manhattanDistance :: (Int, Int) -> Int
-manhattanDistance (x, y) = abs x + abs y
+manhattanDistance :: Coord -> Int
+manhattanDistance (Coord x y) = abs x + abs y
