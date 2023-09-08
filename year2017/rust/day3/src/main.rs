31d4718c4137087040b8916eabc9a9191766b351
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-09-08 21:47:54 +0200

    2017 day3 format

--- year2017/rust/day3/src/main.rs
+++ year2017/rust/day3/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use anyhow::{Result, Ok};
+use anyhow::{Ok, Result};
 
 fn main() -> Result<()> {
     let final_position = 265149;
@@ -15,13 +15,12 @@ fn main() -> Result<()> {
 }
 
 fn part2(final_position: i32) -> i32 {
-
     let mut last_sum = 1;
     let (mut x, mut y): (i32, i32) = (0, 0);
     let (mut dx, mut dy) = (1, 0);
     let mut found = false;
     let mut steps = 1;
-    let mut map = HashMap::from([((0,0),1)]);
+    let mut map = HashMap::from([((0, 0), 1)]);
 
     loop {
         for _ in 1u8..=2 {
@@ -33,7 +32,7 @@ fn part2(final_position: i32) -> i32 {
                     found = true;
                     break;
                 }
-                map.insert((x,y), last_sum);
+                map.insert((x, y), last_sum);
             }
             if found {
                 break;
@@ -48,11 +47,10 @@ fn part2(final_position: i32) -> i32 {
     last_sum
 }
 
-
-fn get_neigbors(x: i32, y: i32, map: &HashMap<(i32,i32), i32>) -> i32 {
+fn get_neigbors(x: i32, y: i32, map: &HashMap<(i32, i32), i32>) -> i32 {
     let mut sum = 0;
-    for _x in x-1..=x+1 {
-        for _y in y-1..=y+1 {
+    for _x in x - 1..=x + 1 {
+        for _y in y - 1..=y + 1 {
             if _x == x && _y == y {
                 continue;
             }
@@ -93,5 +91,5 @@ fn part1(final_position: i32) -> i32 {
     }
 
     let manhattan = x.abs() + y.abs();
-    return manhattan
+    return manhattan;
 }
