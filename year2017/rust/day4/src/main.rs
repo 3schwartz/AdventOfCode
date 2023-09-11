dc2f2ab8df823544983d81181f3ad07257f0e146
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-09-11 20:14:31 +0200

    2017 day4 part2

--- year2017/rust/day4/src/main.rs
+++ year2017/rust/day4/src/main.rs
@@ -1,16 +1,34 @@
-use std::{fs, collections::HashSet};
+use std::{collections::BTreeSet, fs};
 
 use anyhow::Result;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day4_data.txt")?;
-    let mut valid_passphrases = 0;
 
+    let part_1 = get_valid_passphrases(&input, same);
+    println!("Part 1: {}", part_1);
+
+    let part_2 = get_valid_passphrases_rearrange(&input);
+    println!("Part 2: {}", part_2);
+
+    Ok(())
+}
+
+fn same(word: &str) -> &str {
+    word
+}
+
+fn get_valid_passphrases<F>(input: &str, condition: F) -> u32
+where
+    F: for<'a> Fn(&'a str) -> &'a str,
+{
+    let mut valid_passphrases = 0;
     for line in input.lines() {
-        let mut seen = HashSet::new();
+        let mut seen = BTreeSet::new();
         let mut valid = true;
         for word in line.split_whitespace() {
-            if !seen.insert(word) {
+            let lookup = condition(word);
+            if !seen.insert(lookup) {
                 valid = false;
                 break;
             }
@@ -19,8 +37,26 @@ fn main() -> Result<()> {
             valid_passphrases += 1;
         }
     }
-    
-    println!("Part 1: {}", valid_passphrases);
 
-    Ok(())
+    valid_passphrases
+}
+
+fn get_valid_passphrases_rearrange(input: &str) -> u32 {
+    let mut valid_passphrases = 0;
+    for line in input.lines() {
+        let mut seen = BTreeSet::new();
+        let mut valid = true;
+        for word in line.split_whitespace() {
+            let lookup: BTreeSet<char> = word.chars().collect();
+            if !seen.insert(lookup) {
+                valid = false;
+                break;
+            }
+        }
+        if valid {
+            valid_passphrases += 1;
+        }
+    }
+
+    valid_passphrases
 }
