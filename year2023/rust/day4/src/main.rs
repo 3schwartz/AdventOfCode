dfeea300c4d139a5267af52346e2469b1b6b1282
schwartz <40674593+3schwartz@users.noreply.github.co>
2023-12-04 08:30:37 +0100

    2023 day4 part2

--- year2023/rust/day4/src/main.rs
+++ year2023/rust/day4/src/main.rs
@@ -1,6 +1,9 @@
-use std::{collections::HashSet, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
-use anyhow::{Result, Ok};
+use anyhow::{Ok, Result};
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day4_data.txt")?;
@@ -8,24 +11,37 @@ fn main() -> Result<()> {
 
     println!("Part 1: {}", total);
 
+    let total_scratchcards = get_total_scratchcards(&input)?;
+
+    println!("Part 2: {}", total_scratchcards);
+
     Ok(())
+}
+
+fn get_total_scratchcards(input: &str) -> Result<u32> {
+    let mut card_map = HashMap::new();
+    let mut idx_max = 0;
+    for (idx, line) in input.lines().enumerate() {
+        idx_max = idx;
+        let current = *card_map.entry(idx).or_insert(1u32);
+        let intersection = get_intersection(line)?;
+        for i in 1..=intersection.len() {
+            let entry = card_map.entry(i + idx).or_insert(1u32);
+            *entry += current;
+        }
+    }
+    let sum: u32 = card_map
+        .iter()
+        .filter(|(&k, _)| k <= idx_max)
+        .map(|(_, &v)| v)
+        .sum();
+    Ok(sum)
 }
 
 fn get_points(input: &str) -> Result<u32> {
     let mut total = 0;
     for line in input.lines() {
-        let trimmed: Vec<&str> = line.split(": ").collect();
-        let card_split: Vec<&str> = trimmed[1].split(" | ").collect();
-        let win: HashSet<u32> = card_split[0]
-            .split_whitespace()
-            .map(|c| c.parse())
-            .collect::<Result<HashSet<u32>, _>>()?;
-        let numbers: HashSet<u32> = card_split[1]
-            .split_whitespace()
-            .map(|c| c.parse())
-            .collect::<Result<HashSet<u32>, _>>()?;
-        let intersection = numbers.intersection(&win)
-            .collect::<HashSet<&u32>>();
+        let intersection = get_intersection(line)?;
         if intersection.is_empty() {
             continue;
         }
@@ -33,6 +49,24 @@ fn get_points(input: &str) -> Result<u32> {
         total += points;
     }
     Ok(total)
+}
+
+fn get_intersection(line: &str) -> Result<HashSet<u32>> {
+    let trimmed: Vec<&str> = line.split(": ").collect();
+    let card_split: Vec<&str> = trimmed[1].split(" | ").collect();
+    let win: HashSet<u32> = card_split[0]
+        .split_whitespace()
+        .map(|c| c.parse())
+        .collect::<Result<HashSet<u32>, _>>()?;
+    let numbers: HashSet<u32> = card_split[1]
+        .split_whitespace()
+        .map(|c| c.parse())
+        .collect::<Result<HashSet<u32>, _>>()?;
+    let intersection = numbers
+        .intersection(&win)
+        .copied()
+        .collect::<HashSet<u32>>();
+    Ok(intersection)
 }
 
 #[cfg(test)]
@@ -49,6 +83,19 @@ mod test {
 
         // Assert
         assert_eq!(13, total);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_2() -> Result<()> {
+        // Arrange
+        let input = fs::read_to_string("../../data/day4_data test.txt")?;
+
+        // Act
+        let total = get_total_scratchcards(&input)?;
+
+        // Assert
+        assert_eq!(30, total);
         Ok(())
     }
 }
