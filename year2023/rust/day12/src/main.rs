cc55b4d590ea17922e1f3088d91a38dd538aee4b
3schwartz <40674593+3schwartz@users.noreply.github.com>
2023-12-26 17:42:30 +0100

    2023 day12 format

--- year2023/rust/day12/src/main.rs
+++ year2023/rust/day12/src/main.rs
@@ -18,7 +18,7 @@ fn find_combinations(input: &str, count: u32) -> Result<i64> {
 
     let mut combinations = 0;
     for line in input.lines() {
-        let state = State::from(&line, count)?;
+        let state = State::from(line, count)?;
         combinations += state.find_next_combinations(&mut cache);
     }
 
@@ -33,12 +33,12 @@ struct State {
 
 impl State {
     fn from(input: &str, repeats: u32) -> Result<Self> {
-        let parts: Vec<&str> = input.split(" ").collect();
+        let parts: Vec<&str> = input.split(' ').collect();
         if parts.len() != 2 {
             return Err(anyhow!("{} not able to split by space", input));
         };
         let group = parts[1]
-            .split(",")
+            .split(',')
             .map(|c| c.parse())
             .collect::<Result<Vec<i64>, _>>()?;
         let mut sequences = vec![];
@@ -50,7 +50,7 @@ impl State {
             count += 1;
         }
 
-        let groups = g.iter().flatten().map(|c| *c).collect();
+        let groups = g.iter().flatten().copied().collect();
         let sequence = sequences.join("?");
 
         Ok(Self { sequence, groups })
@@ -69,17 +69,17 @@ impl State {
     /// operational, '.' elements.
     /// If it doesn't they can be used as a damage group.
     fn next_slice_damaged_or_unknown(&self) -> bool {
-        !self.sequence[..self.groups[0] as usize].contains(".")
+        !self.sequence[..self.groups[0] as usize].contains('.')
     }
 
-    fn find_next_combinations<'b>(&self, cache: &'b mut BTreeMap<State, i64>) -> i64 {
+    fn find_next_combinations(&self, cache: &mut BTreeMap<State, i64>) -> i64 {
         if let Some(c) = cache.get(self) {
             return *c;
         };
         // When no groups are left check if there are none
         // damage left.
         if self.groups.is_empty() {
-            return !self.sequence.contains("#") as i64;
+            return !self.sequence.contains('#') as i64;
         }
         if self.is_group_size_below_remaining() {
             return 0;
@@ -92,7 +92,7 @@ impl State {
         // When first isn't '#' it can be either '.' or a '?' which
         // can be used as a '?'.
         let first = if &self.sequence[0..1] != "#" {
-            let c = &self.sequence[1..].trim_start_matches(".");
+            let c = &self.sequence[1..].trim_start_matches('.');
             let n = State {
                 sequence: c.to_string(),
                 groups: self.groups.clone(),
@@ -109,7 +109,7 @@ impl State {
         let second = if next_slice_damaged_or_unknown
             && &self.sequence[self.groups[0] as usize..self.groups[0] as usize + 1] != "#"
         {
-            let c = &self.sequence[self.groups[0] as usize + 1..].trim_start_matches(".");
+            let c = &self.sequence[self.groups[0] as usize + 1..].trim_start_matches('.');
             let v = self.groups.iter().skip(1).copied().collect();
             let s = State {
                 sequence: c.to_string(),
