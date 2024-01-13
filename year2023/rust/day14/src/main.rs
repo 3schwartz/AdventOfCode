3cb7a52421726806a1b906af21f98e8476ce8df7
3schwartz <40674593+3schwartz@users.noreply.github.com>
2024-01-13 16:20:20 +0100

    2023 day14 clippy

--- year2023/rust/day14/src/main.rs
+++ year2023/rust/day14/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::BTreeMap};
+use std::{collections::BTreeMap, fs};
 
 use anyhow::Result;
 
@@ -8,8 +8,8 @@ fn tilt(grid: &mut Vec<Vec<char>>) {
     for c in 0..cols {
         for _ in 0..rows {
             for r in 0..rows {
-                if r > 0 && grid[r][c] == 'O' && grid[r-1][c] == '.' {
-                    grid[r-1][c] = 'O';
+                if r > 0 && grid[r][c] == 'O' && grid[r - 1][c] == '.' {
+                    grid[r - 1][c] = 'O';
                     grid[r][c] = '.';
                 }
             }
@@ -19,12 +19,10 @@ fn tilt(grid: &mut Vec<Vec<char>>) {
 
 fn score(grid: &Vec<Vec<char>>) -> usize {
     let rows = grid.len();
-    let cols = grid[0].len();
     let mut score = 0;
-    for r in 0..rows {
-        let row = &grid[r];
-        for c in 0..cols {
-            if row[c] == 'O' {
+    for (r, row) in grid.iter().enumerate() {
+        for col in row.iter() {
+            if *col == 'O' {
                 score += rows - r;
             }
         }
@@ -42,7 +40,7 @@ fn empty_grid(rows: usize, cols: usize) -> Vec<Vec<char>> {
 }
 
 /// Rotate counter clockwise in coordinate system is going from
-/// (x,y) to (y,-x). In our case it will be going from 
+/// (x,y) to (y,-x). In our case it will be going from
 /// [row][column], (y,x) to [-column][row] , (-x,y).
 /// To only have positive coordinate we shift the [row] index
 /// by R - 1 - [column].
@@ -50,14 +48,12 @@ fn rotate(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     let rows = grid.len();
     let cols = grid[0].len();
     let mut empty = empty_grid(cols, rows);
-    //println!("{:?}", empty);
-    for r in 0..rows {
-        for c in 0..cols {
-            empty[rows -1 - c][r] = grid[r][c];
+    for (r, row) in grid.iter().enumerate() {
+        for (c, item) in row.iter().enumerate() {
+            empty[c][rows - 1 - r] = *item;
         }
     }
     empty
-
 }
 /// Stores as [row][column] which in a coordinate system
 /// can be interpreted as [y][x].
@@ -72,14 +68,11 @@ fn make_grid_from_input(input: &str) -> Vec<Vec<char>> {
     grid
 }
 
-fn create_cache_key(grid: &Vec<Vec<char>>) -> BTreeMap<(usize, usize), char> {
-    let rows = grid.len();
-    let cols = grid[0].len();
+fn create_cache_key(grid: &[Vec<char>]) -> BTreeMap<(usize, usize), char> {
     let mut key = BTreeMap::new();
-    for r in 0..rows {
-        let row = &grid[r];
-        for c in 0..cols {
-            key.insert((r,c), row[c]);
+    for (r, row) in grid.iter().enumerate() {
+        for (c, col) in row.iter().enumerate() {
+            key.insert((r, c), *col);
         }
     }
     key
@@ -87,7 +80,7 @@ fn create_cache_key(grid: &Vec<Vec<char>>) -> BTreeMap<(usize, usize), char> {
 
 fn run_cycles(total_cycles: u32, mut grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let mut cycles = 0;
-    let mut cache: BTreeMap<BTreeMap<(usize,usize), char>, u32> = BTreeMap::new();
+    let mut cache: BTreeMap<BTreeMap<(usize, usize), char>, u32> = BTreeMap::new();
     while cycles < total_cycles {
         cycles += 1;
         for _ in 0..4 {
@@ -107,28 +100,26 @@ fn run_cycles(total_cycles: u32, mut grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
 }
 
 fn part_2(input: &str) -> usize {
-    let mut grid = make_grid_from_input(&input);
+    let mut grid = make_grid_from_input(input);
     grid = run_cycles(1_000_000_000, grid);
-    let score = score(&grid);
-    score
+    score(&grid)
 }
 
 fn part_1(input: &str) -> usize {
-    let mut grid = make_grid_from_input(&input);
+    let mut grid = make_grid_from_input(input);
     tilt(&mut grid);
-    let score = score(&grid);
-    score
+    score(&grid)
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day14_data.txt")?;
 
     let part_1 = part_1(&input);
-    
+
     println!("Part 1: {}", part_1);
 
     let part_2 = part_2(&input);
-    
+
     println!("Part 2: {}", part_2);
 
     Ok(())
