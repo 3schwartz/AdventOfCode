b6ec81080a7f8f10e93f74388e9e57d0661f1ab5
3schwartz <40674593+3schwartz@users.noreply.github.com>
2024-01-13 16:07:35 +0100

    2023 day14 part2

--- year2023/rust/day14/src/main.rs
+++ year2023/rust/day14/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::BTreeMap};
 
 use anyhow::Result;
 
@@ -32,6 +32,35 @@ fn score(grid: &Vec<Vec<char>>) -> usize {
     score
 }
 
+fn empty_grid(rows: usize, cols: usize) -> Vec<Vec<char>> {
+    let mut grid = vec![];
+    for _ in 0..rows {
+        let row: Vec<char> = vec![Default::default(); cols];
+        grid.push(row);
+    }
+    grid
+}
+
+/// Rotate counter clockwise in coordinate system is going from
+/// (x,y) to (y,-x). In our case it will be going from 
+/// [row][column], (y,x) to [-column][row] , (-x,y).
+/// To only have positive coordinate we shift the [row] index
+/// by R - 1 - [column].
+fn rotate(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut empty = empty_grid(cols, rows);
+    //println!("{:?}", empty);
+    for r in 0..rows {
+        for c in 0..cols {
+            empty[rows -1 - c][r] = grid[r][c];
+        }
+    }
+    empty
+
+}
+/// Stores as [row][column] which in a coordinate system
+/// can be interpreted as [y][x].
 fn make_grid_from_input(input: &str) -> Vec<Vec<char>> {
     let mut grid: Vec<Vec<char>> = vec![];
     for (row, line) in input.lines().enumerate() {
@@ -43,14 +72,64 @@ fn make_grid_from_input(input: &str) -> Vec<Vec<char>> {
     grid
 }
 
-fn main() -> Result<()> {
-    let input = fs::read_to_string("../data/day14_data.txt")?;
+fn create_cache_key(grid: &Vec<Vec<char>>) -> BTreeMap<(usize, usize), char> {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut key = BTreeMap::new();
+    for r in 0..rows {
+        let row = &grid[r];
+        for c in 0..cols {
+            key.insert((r,c), row[c]);
+        }
+    }
+    key
+}
 
+fn run_cycles(total_cycles: u32, mut grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut cycles = 0;
+    let mut cache: BTreeMap<BTreeMap<(usize,usize), char>, u32> = BTreeMap::new();
+    while cycles < total_cycles {
+        cycles += 1;
+        for _ in 0..4 {
+            tilt(&mut grid);
+            grid = rotate(&grid);
+        }
+        let key = create_cache_key(&grid);
+        if let Some(cycle) = cache.get(&key) {
+            let period = cycles - cycle;
+            let remaining = (total_cycles - cycles) / period;
+            cycles += remaining * period;
+        }
+        cache.insert(key, cycles);
+    }
+
+    grid
+}
+
+fn part_2(input: &str) -> usize {
+    let mut grid = make_grid_from_input(&input);
+    grid = run_cycles(1_000_000_000, grid);
+    let score = score(&grid);
+    score
+}
+
+fn part_1(input: &str) -> usize {
     let mut grid = make_grid_from_input(&input);
     tilt(&mut grid);
     let score = score(&grid);
+    score
+}
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("../data/day14_data.txt")?;
+
+    let part_1 = part_1(&input);
     
-    println!("Part 1: {}", score);
+    println!("Part 1: {}", part_1);
+
+    let part_2 = part_2(&input);
+    
+    println!("Part 2: {}", part_2);
 
     Ok(())
 }
