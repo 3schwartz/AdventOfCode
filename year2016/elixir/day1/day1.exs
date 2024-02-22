e49811940a927e44b2fc2b40ef97fb870c7e4524
3schwartz <40674593+3schwartz@users.noreply.github.com>
2024-02-22 11:42:29 +0100

    2016 day1 part 2 elixir

--- year2016/elixir/day1/day1.exs
+++ year2016/elixir/day1/day1.exs
@@ -7,3 +7,7 @@ lines =
 {{x, y}, _} = Enum.reduce(lines, {{0, 0}, {0, 1}}, fn move, state -> Day1.apply_action(move, state) end)
 
 IO.puts("Part 1: #{Day1.manhattan_distance({x, y})}")
+
+{{x, y}, _, _} = Enum.reduce_while(lines, {{0, 0}, {0, 1}, MapSet.new()}, fn move, state -> Day1.apply_action_with_set(move, state) end)
+
+IO.puts("Part 2: #{Day1.manhattan_distance({x, y})}")
--- year2016/elixir/day1/lib/day1.ex
+++ year2016/elixir/day1/lib/day1.ex
@@ -29,4 +29,21 @@ defmodule Day1 do
     {{pxn, pyn}, {dxn, dyn}}
   end
 
+  @spec apply_action_with_set(
+          binary(),
+          {{number(), number()}, {number(), number()}, MapSet.t({number(), number()})}
+        ) ::
+          {:cont, {{number(), number()}, {number(), number()}, MapSet.t({number(), number()})}}
+          | {:halt, {{number(), number()}, {number(), number()}, MapSet.t({number(), number()})}}
+  def apply_action_with_set(move, {{px, py}, {dx, dy}, set}) do
+    {dxn, dyn} = rotate(move, {dx, dy})
+    {pxn, pyn} = apply_movement(move, {px, py}, {dxn, dyn})
+
+    if MapSet.member?(set, {pxn, pyn}) do
+      {:halt, {{pxn, pyn}, {dxn, dyn}, set}}
+    else
+      {:cont, {{pxn, pyn}, {dxn, dyn}, MapSet.put(set, {pxn, pyn})}}
+    end
+  end
+
 end
