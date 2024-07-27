e999edacc856c79624a5a504f46081cb0c31dbde
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-07-27 17:39:00 +0200

    2017 day10 part2

--- year2017/rust/day10/src/main.rs
+++ year2017/rust/day10/src/main.rs
@@ -9,23 +9,60 @@ fn main() -> Result<()> {
         .collect::<Result<Vec<usize>, _>>()?;
 
     let mut list: Vec<u8> = (0..=255).collect();
-    let list_length = 256;
 
     let mut cursor = 0;
+    let mut skip_size = 0;
 
-    for (skip_size, length) in lengths.iter().copied().enumerate() {
+    execute_round(&mut list, &lengths, &mut cursor, &mut skip_size);
+    println!("Part 1: {}", list[0] as u32 * list[1] as u32);
+
+    let mut lengths_ascii: Vec<usize> = input
+        .trim()
+        .chars()
+        .map(|a| a as u8)
+        .map(|a| a as usize)
+        .collect();
+    lengths_ascii.extend_from_slice(&[17, 31, 73, 47, 23]);
+
+    let mut list: Vec<u8> = (0..=255).collect();
+    let mut cursor = 0;
+    let mut skip_size = 0;
+    for _ in 0..64 {
+        execute_round(&mut list, &lengths_ascii, &mut cursor, &mut skip_size);
+    }
+
+    assert!(list.len() % 16 == 0);
+    let mut elements = Vec::with_capacity(16);
+    for chunk in list.chunks(16) {
+        let xored = chunk.iter().fold(0, |acc, &x| acc ^ x);
+        elements.push(xored)
+    }
+    let hex_string: String = elements.iter().map(|x| format!("{:02x}", x)).collect();
+
+    println!("{}", hex_string);
+
+    Ok(())
+}
+
+fn execute_round(
+    list: &mut Vec<u8>,
+    lengths: &Vec<usize>,
+    cursor: &mut usize,
+    skip_size: &mut usize,
+) {
+    let list_length = list.len();
+
+    for length in lengths.iter().copied() {
         let mut to_reverse: Vec<u8> = Vec::with_capacity(length);
         for j in 0..length {
-            to_reverse.push(list[(j + cursor) % list_length])
+            to_reverse.push(list[(j + *cursor) % list_length])
         }
         to_reverse.reverse();
         for (j, reverse) in to_reverse.iter().enumerate() {
-            list[(j + cursor) % list_length] = *reverse;
+            list[(j + *cursor) % list_length] = *reverse;
         }
 
-        cursor = (cursor + skip_size + length) % list_length;
+        *cursor = (*cursor + *skip_size + length) % list_length;
+        *skip_size += 1;
     }
-
-    println!("Part 1: {}", list[0] as u32 * list[1] as u32);
-    Ok(())
 }
