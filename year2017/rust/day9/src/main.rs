31f3b2cdcc9e563408b627d9ac850ee437bfcd75
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-07-27 16:14:14 +0200

    2017 day9 part1

---
+++ year2017/rust/day9/src/main.rs
@@ -0,0 +1,45 @@
+use anyhow::Result;
+use std::fs;
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("../data/day9_data.txt")?;
+
+    let mut is_garbage = false;
+    let mut discard = false;
+    let mut debt = 0;
+    let mut sum = 0;
+
+    for c in input.chars() {
+        if discard {
+            discard = false;
+            continue;
+        }
+        if c == '!' {
+            discard = true;
+            continue;
+        }
+        if is_garbage && c != '>' {
+            continue;
+        }
+        if is_garbage && c == '>' {
+            is_garbage = false;
+            continue;
+        }
+        if !is_garbage && c == '<' {
+            is_garbage = true;
+            continue;
+        }
+        if c == '{' {
+            debt += 1;
+            continue;
+        }
+        if c == '}' {
+            sum += debt;
+            debt -= 1;
+        }
+        continue;
+    }
+
+    println!("Part 1: {}", sum);
+    Ok(())
+}
