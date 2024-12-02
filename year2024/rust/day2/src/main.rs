bf9ccb1f8cbcf3e45521d6c320540c56783259d9
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-12-02 07:41:54 +0100

    2024 day2 part2

--- year2024/rust/day2/src/main.rs
+++ year2024/rust/day2/src/main.rs
@@ -5,7 +5,8 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day2_data.txt")?;
 
-    let mut total_safe = 0;
+    let mut total_safe_part_1 = 0;
+    let mut total_safe_part_2 = 0;
 
     for line in input.lines() {
         let parts = line
@@ -13,40 +14,49 @@ fn main() -> Result<()> {
             .map(|n| n.parse())
             .collect::<Result<Vec<i32>, _>>()?;
 
-        let mut is_safe = true;
-        let mut is_negative = None;
+        if is_line_save(&parts) {
+            total_safe_part_1 += 1;
+            total_safe_part_2 += 1;
+            continue;
+        }
 
-        for i in 1..parts.len() {
-            // if i > parts.len() - 1
-            //     && (parts[i - 1] < parts[i] && parts[i] > parts[i + 1]
-            //         || parts[i - 1] < parts[i] && parts[i] > parts[i + 1])
-            // {
-            //     is_safe = false;
-            //     break;
-            // }
+        for i in 0..parts.len() {
+            let copied: Vec<i32> = parts
+                .iter()
+                .enumerate()
+                .filter(|&(index, _)| index != i)
+                .map(|(_, &value)| value)
+                .collect();
 
-            let direction = parts[i - 1] - parts[i];
-            if let Some(prior_direction) = is_negative {
-                if prior_direction != (direction < 0) {
-                    is_safe = false;
-                    break;
-                }
-            } else {
-                is_negative = Some(direction < 0);
-            }
-
-            if parts[i - 1] == parts[i] || (parts[i - 1] - parts[i]).abs() > 3 {
-                is_safe = false;
+            if is_line_save(&copied) {
+                total_safe_part_2 += 1;
                 break;
             }
         }
-
-        if is_safe {
-            total_safe += 1;
-        }
     }
 
-    println!("Part 1: {}", total_safe);
+    println!("Part 1: {}", total_safe_part_1);
+    println!("Part 2: {}", total_safe_part_2);
 
     Ok(())
+}
+
+fn is_line_save(parts: &Vec<i32>) -> bool {
+    let mut is_negative = None;
+
+    for i in 1..parts.len() {
+        let direction = parts[i - 1] - parts[i];
+        if let Some(prior_direction) = is_negative {
+            if prior_direction != (direction < 0) {
+                return false;
+            }
+        } else {
+            is_negative = Some(direction < 0);
+        }
+
+        if parts[i - 1] == parts[i] || (parts[i - 1] - parts[i]).abs() > 3 {
+            return false;
+        }
+    }
+    return true;
 }
