771b3328c0ad7951b353280a154b3713f530ee79
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-12-19 12:52:48 +0100

    2024 day19 part1

--- year2024/rust/day18/src/main.rs
+++ year2024/rust/day18/src/main.rs
@@ -1,14 +1,83 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::fs;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day18_data.txt")?;
 
-    for line in input.lines() {
-        println!("{}", line);
+    let memory = MemorySpace::new(&input, 1024, 70, 70)?;
+    let steps = memory.steps().unwrap();
+
+    println!("Part 1: {}", steps);
+
+    let coord = MemorySpace::find_non_reachable(&input, 70, 70)?;
+
+    println!("Part 2: {coord}");
+    Ok(())
+}
+
+use std::collections::{HashSet, VecDeque};
+
+struct MemorySpace {
+    x: i32,
+    y: i32,
+    corrupt: HashSet<(i32, i32)>,
+}
+
+impl MemorySpace {
+    const N: [(i32, i32); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+
+    fn find_non_reachable(input: &str, x: i32, y: i32) -> Result<String> {
+        let coords = input.lines().collect::<Vec<&str>>();
+        for i in 1..=coords.len() {
+            let memory = MemorySpace::new(&input, i, x, y)?;
+            if memory.steps().is_some() {
+                continue;
+            };
+            return Ok(coords[i - 1].to_string());
+        }
+        Err(anyhow!("no result found"))
     }
 
-    Ok(())
+    fn new(input: &str, size: usize, x: i32, y: i32) -> Result<Self> {
+        let mut corrupt = HashSet::new();
+        for (i, line) in input.lines().enumerate() {
+            if i == size {
+                break;
+            }
+            let parts = line
+                .split(',')
+                .map(|n| n.parse())
+                .collect::<Result<Vec<i32>, _>>()?;
+            assert_eq!(parts.len(), 2);
+            corrupt.insert((parts[0], parts[1]));
+        }
+        Ok(Self { x, y, corrupt })
+    }
+
+    fn steps(&self) -> Option<u32> {
+        let mut queue = VecDeque::from([(0, (0, 0))]);
+        let mut seen = HashSet::new();
+
+        while let Some((steps, (x, y))) = queue.pop_front() {
+            if !seen.insert((x, y)) {
+                continue;
+            }
+            if x == self.x && y == self.y {
+                return Some(steps);
+            }
+            if self.corrupt.contains(&(x, y)) {
+                continue;
+            }
+            for neighbor in Self::N {
+                let n = (neighbor.0 + x, neighbor.1 + y);
+                if n.0 < 0 || n.0 > self.x || n.1 < 0 || n.1 > self.y {
+                    continue;
+                }
+                queue.push_back((steps + 1, n));
+            }
+        }
+        None
+    }
 }
 
 #[cfg(test)]
@@ -18,16 +87,27 @@ mod test {
     #[test]
     fn test_part_1() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day18_test_data.txt")?;
+
         // Act
+        let memory = MemorySpace::new(&input, 12, 6, 6)?;
+        let steps = memory.steps();
+
         // Assert
+        assert_eq!(steps.unwrap(), 22);
         Ok(())
     }
 
     #[test]
     fn test_part_2() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day18_test_data.txt")?;
+
         // Act
+        let coord = MemorySpace::find_non_reachable(&input, 6, 6)?;
+
         // Assert
+        assert_eq!(coord, "6,1");
         Ok(())
     }
 }
--- year2024/rust/day19/src/main.rs
+++ year2024/rust/day19/src/main.rs
@@ -1,14 +1,69 @@
+use anyhow::Ok;
 use anyhow::Result;
 use std::fs;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day19_data.txt")?;
 
-    for line in input.lines() {
-        println!("{}", line);
-    }
+    let onsen = Onsen::from_str(&input)?;
+    let possible = onsen.possible_designs();
+
+    println!("Part 1: {possible}");
 
     Ok(())
+}
+
+use std::{collections::HashSet, str::FromStr};
+
+struct Onsen {
+    patterns: HashSet<String>,
+    designs: Vec<String>,
+}
+
+impl Onsen {
+    fn possible_designs(&self) -> u32 {
+        let mut possible = 0;
+        for design in &self.designs {
+            if self.check_design(design) {
+                possible += 1;
+            }
+        }
+        possible
+    }
+
+    fn check_design(&self, design: &str) -> bool {
+        if design == "" {
+            return true;
+        }
+        for i in 1..=design.len() {
+            let sub_design = &design[0..i];
+            if !self.patterns.contains(sub_design) {
+                continue;
+            }
+            if self.check_design(&design[i..]) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+impl FromStr for Onsen {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let parts = s.trim().split("\n\n").collect::<Vec<&str>>();
+        assert_eq!(parts.len(), 2);
+
+        let patterns = parts[0]
+            .split(", ")
+            .map(|n| n.to_string())
+            .collect::<HashSet<String>>();
+        let designs = parts[1].lines().map(|n| n.to_string()).collect();
+
+        Ok(Self { patterns, designs })
+    }
 }
 
 #[cfg(test)]
@@ -16,10 +71,27 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_splice() {
+        let input = "hello";
+        let part = &input[0..1];
+
+        assert_eq!(part, "h");
+        assert_eq!(&input[4..5], "o");
+        assert_eq!(&input[5..5], "");
+        assert_eq!(&input[5..], "");
+    }
+
+    #[test]
     fn test_part_1() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day19_test_data.txt")?;
+
         // Act
+        let onsen = Onsen::from_str(&input)?;
+        let possible = onsen.possible_designs();
+
         // Assert
+        assert_eq!(possible, 6);
         Ok(())
     }
 
