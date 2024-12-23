f29d6437e0fe6463a1b03f24db1926f287bd6462
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2024-12-23 21:27:10 +0100

    2024 day23

--- year2024/rust/day22/src/main.rs
+++ year2024/rust/day22/src/main.rs
@@ -1,14 +1,101 @@
-use anyhow::Result;
-use std::fs;
+use anyhow::{anyhow, Ok, Result};
+use std::{collections::HashMap, fs};
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day22_data.txt")?;
 
-    for line in input.lines() {
-        println!("{}", line);
-    }
+    let secret_sum = find_secret_sum(&input)?;
+
+    println!("Part 1: {secret_sum}");
+
+    let bananas = find_bananas(&input)?;
+
+    println!("Part 2: {bananas}");
 
     Ok(())
+}
+
+fn find_secret_sum(input: &str) -> Result<u64> {
+    let mut secret_sum = 0;
+    for line in input.lines() {
+        let secrets = secrets(line)?;
+        secret_sum += secrets[secrets.len() - 1];
+    }
+    Ok(secret_sum)
+}
+
+fn find_bananas(input: &str) -> Result<i64> {
+    let mut sequence_prices = HashMap::new();
+    for line in input.lines() {
+        let secrets = secrets(line)?;
+        let prices = prices(&secrets);
+        let changes = changes(&prices);
+        let sequences = sequences(&changes, &prices);
+        for (sequence, price) in sequences {
+            sequence_prices
+                .entry(sequence)
+                .and_modify(|p| *p += price)
+                .or_insert(price);
+        }
+    }
+    sequence_prices
+        .values()
+        .max()
+        .copied()
+        .ok_or_else(|| anyhow!("expected max to be found"))
+}
+
+fn sequences(changes: &[i64], prices: &[i64]) -> HashMap<(i64, i64, i64, i64), i64> {
+    let mut sequences = HashMap::new();
+    for i in 3..changes.len() {
+        let change = (changes[i - 3], changes[i - 2], changes[i - 1], changes[i]);
+        if sequences.contains_key(&change) {
+            continue;
+        }
+        let price = prices[i + 1];
+        sequences.insert(change, price);
+    }
+    sequences
+}
+
+fn changes(prices: &[i64]) -> Vec<i64> {
+    let mut changes = vec![];
+    for i in 1..prices.len() {
+        changes.push(prices[i] - prices[i - 1]);
+    }
+    changes
+}
+
+fn prices(secrets: &[u64]) -> Vec<i64> {
+    secrets.iter().map(|s| s % 10).map(|p| p as i64).collect()
+}
+
+fn secrets(line: &str) -> Result<Vec<u64>> {
+    let mut secrets = vec![];
+    let mut secret = line.parse::<u64>()?;
+    for _ in 0..2_000 {
+        let result = secret * 64;
+        secret = mix(secret, result);
+        secret = prune(secret);
+
+        let result = secret / 32;
+        secret = mix(secret, result);
+        secret = prune(secret);
+
+        let result = secret * 2_048;
+        secret = mix(secret, result);
+        secret = prune(secret);
+        secrets.push(secret);
+    }
+    Ok(secrets)
+}
+
+fn mix(secret: u64, result: u64) -> u64 {
+    secret ^ result
+}
+
+fn prune(secret: u64) -> u64 {
+    secret % 16_777_216
 }
 
 #[cfg(test)]
@@ -18,16 +105,26 @@ mod test {
     #[test]
     fn test_part_1() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day22_test_data.txt")?;
+
         // Act
+        let secret_sum = find_secret_sum(&input)?;
+
         // Assert
+        assert_eq!(secret_sum, 37_327_623);
         Ok(())
     }
 
     #[test]
     fn test_part_2() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day22_test2_data.txt")?;
+
         // Act
+        let bananas = find_bananas(&input)?;
+
         // Assert
+        assert_eq!(bananas, 23);
         Ok(())
     }
 }
--- year2024/rust/day23/src/main.rs
+++ year2024/rust/day23/src/main.rs
@@ -1,14 +1,97 @@
 use anyhow::Result;
+use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
 use std::fs;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("../data/day23_data.txt")?;
+    let connections = make_connections(&input);
+    let sets = make_sets(&connections);
+    let filtered = sets
+        .iter()
+        .filter(|s| s.len() == 3)
+        .filter(|s| s.iter().any(|n| n.starts_with('t')))
+        .count();
 
-    for line in input.lines() {
-        println!("{}", line);
-    }
+    println!("Part 1: {filtered}");
+
+    let best = find_best(&connections);
+
+    println!("Part 2: {best}");
 
     Ok(())
+}
+
+fn make_connections(input: &str) -> HashMap<String, HashSet<String>> {
+    let mut connections: HashMap<String, HashSet<String>> = HashMap::new();
+
+    for line in input.lines() {
+        let parts = line.split('-').collect::<Vec<&str>>();
+        assert_eq!(parts.len(), 2);
+        connections
+            .entry(parts[0].to_string())
+            .and_modify(|c| {
+                c.insert(parts[1].to_string());
+            })
+            .or_insert_with(|| HashSet::from([parts[1].to_string()]));
+        connections
+            .entry(parts[1].to_string())
+            .and_modify(|c| {
+                c.insert(parts[0].to_string());
+            })
+            .or_insert_with(|| HashSet::from([parts[0].to_string()]));
+    }
+    connections
+}
+
+fn make_sets(connections: &HashMap<String, HashSet<String>>) -> BTreeSet<BTreeSet<&String>> {
+    let indexable: Vec<(&String, &HashSet<String>)> = connections.iter().collect();
+
+    let mut sets = BTreeSet::new();
+
+    for (x, (s_1, _)) in indexable.iter().enumerate() {
+        for y in x + 1..indexable.len() {
+            for z in y + 1..indexable.len() {
+                let (s_2, set_2) = indexable[y];
+                let (s_3, set_3) = indexable[z];
+                if set_2.contains(*s_1) && set_3.contains(*s_1) && set_3.contains(s_2) {
+                    sets.insert(BTreeSet::from([s_1, s_2, s_3]));
+                }
+            }
+        }
+    }
+    sets
+}
+
+fn find_best(connections: &HashMap<String, HashSet<String>>) -> String {
+    let mut groups: BTreeSet<BTreeSet<&String>> = BTreeSet::new();
+    let mut seen = BTreeSet::new();
+    for k in connections.keys() {
+        let mut queue = VecDeque::from([(k, BTreeSet::from([k]))]);
+        while let Some((prior, prior_group)) = queue.pop_front() {
+            if !seen.insert(prior_group.clone()) {
+                continue;
+            }
+            let prior_connects = connections.get(prior).unwrap();
+            for prior_connect in prior_connects {
+                if prior_group.contains(prior_connect) {
+                    groups.insert(prior_group.clone());
+                    continue;
+                }
+                let next_connects = connections.get(prior_connect).unwrap();
+                if prior_group.iter().all(|pg| next_connects.contains(*pg)) {
+                    let mut cloned = prior_group.clone();
+                    cloned.insert(prior_connect);
+                    queue.push_back((prior_connect, cloned));
+                }
+            }
+        }
+    }
+    let max_group = groups.iter().max_by_key(|group| group.len()).unwrap();
+    max_group
+        .iter()
+        .map(|s| s.as_str())
+        .collect::<Vec<_>>()
+        .join(",")
 }
 
 #[cfg(test)]
@@ -18,16 +101,33 @@ mod test {
     #[test]
     fn test_part_1() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day23_test_data.txt")?;
         // Act
+
+        let connections = make_connections(&input);
+        let sets = make_sets(&connections);
+        let filtered = sets
+            .iter()
+            .filter(|s| s.len() == 3)
+            .filter(|s| s.iter().any(|n| n.starts_with('t')))
+            .count();
+
         // Assert
+        assert_eq!(filtered, 7);
         Ok(())
     }
 
     #[test]
     fn test_part_2() -> Result<()> {
         // Arrange
+        let input = fs::read_to_string("../../data/day23_test_data.txt")?;
         // Act
+
+        let connections = make_connections(&input);
+        let best = find_best(&connections);
+
         // Assert
+        assert_eq!(best, "co,de,ka,ta");
         Ok(())
     }
 }
