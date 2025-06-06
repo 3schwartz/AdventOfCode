42666aff95e34504804a754bb3d963c2ed8f2441
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2025-06-06 09:56:22 +0200

    2016 day2 part 2 c++

--- year2016/cpp/day2/day2.cpp
+++ year2016/cpp/day2/day2.cpp
@@ -1,39 +1,137 @@
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
-using namespace std;
+using std::format;
+using std::invalid_argument;
+using std::pair;
+using std::string;
+using std::vector;
+using std::cout;
+using std::ifstream;
+using std::getline;
+using std::endl;
+using std::map;
 
-struct Keypad
-{
+class IKeyboard {
+protected:
+  ~IKeyboard() = default;
+
+public:
+  virtual string get_key() = 0;
+
+  virtual void move(char direction) = 0;
+};
+
+class ComplexKeyboard final : public IKeyboard {
+public:
+  ComplexKeyboard() : position(0, 2) {
+    keymap = {
+      {pair{2, 0}, "1"},
+      {pair{1, 1}, "2"},
+      {pair{2, 1}, "3"},
+      {pair{3, 1}, "4"},
+      {pair{0, 2}, "5"},
+      {pair{1, 2}, "6"},
+      {pair{2, 2}, "7"},
+      {pair{3, 2}, "8"},
+      {pair{4, 2}, "9"},
+      {pair{1, 3}, "A"},
+      {pair{2, 3}, "B"},
+      {pair{3, 3}, "C"},
+      {pair{2, 4}, "D"}
+    };
+  }
+
+private:
   pair<int, int> position;
-  Keypad() : position(1, 1) {}
+  map<pair<int, int>, string> keymap;
 
-  void move(char direction)
-  {
-    switch (direction)
-    {
-    case 'U':
-      position.second = position.second > 0 ? position.second - 1 : position.second;
-      break;
-    case 'D':
-      position.second = position.second < 2 ? position.second + 1 : position.second;
-      break;
-    case 'L':
-      position.first = position.first > 0 ? position.first - 1 : position.first;
-      break;
-    case 'R':
-      position.first = position.first < 2 ? position.first + 1 : position.first;
-      break;
-    default:
-      throw invalid_argument(format("Invalid instruction: {}", direction));
+public:
+  string get_key() override {
+    return keymap.at(position);
+  }
+
+  void move(char direction) override {
+    switch (direction) {
+      case 'U':
+        if (position.first == 0 && position.second == 2 ||
+            position.first == 1 && position.second == 1 ||
+            position.first == 2 && position.second == 0 ||
+            position.first == 3 && position.second == 1 ||
+            position.first == 4 && position.second == 2) {
+          break;
+        }
+        position.second--;
+        break;
+      case 'D':
+        if (position.first == 0 && position.second == 2 ||
+            position.first == 1 && position.second == 3 ||
+            position.first == 2 && position.second == 4 ||
+            position.first == 3 && position.second == 3 ||
+            position.first == 4 && position.second == 2) {
+          break;
+        }
+        position.second++;
+        break;
+      case 'R':
+        if (position.first == 2 && position.second == 0 ||
+            position.first == 3 && position.second == 1 ||
+            position.first == 4 && position.second == 2 ||
+            position.first == 3 && position.second == 3 ||
+            position.first == 2 && position.second == 4) {
+          break;
+        }
+        position.first++;
+        break;
+      case 'L':
+        if (position.first == 2 && position.second == 0 ||
+            position.first == 1 && position.second == 1 ||
+            position.first == 0 && position.second == 2 ||
+            position.first == 1 && position.second == 3 ||
+            position.first == 2 && position.second == 4) {
+          break;
+        }
+        position.first--;
+        break;
+      default:
+        throw std::invalid_argument(format("Invalid instruction: {}", direction));
+    }
+  }
+};
+
+class SimpleKeypad final : public IKeyboard {
+public:
+  SimpleKeypad() : position(1, 1) {
+  }
+
+private:
+  pair<int, int> position;
+
+public:
+  void move(char direction) override {
+    switch (direction) {
+      case 'U':
+        position.second = std::max(0, position.second - 1);
+        break;
+      case 'D':
+        position.second = std::min(2, position.second + 1);
+        break;
+      case 'L':
+        position.first = std::max(0, position.first - 1);
+        break;
+      case 'R':
+        position.first = std::min(2, position.first + 1);
+        break;
+      default:
+        throw std::invalid_argument(format("Invalid instruction: {}", direction));
     }
   }
 
-  string get_number()
-  {
+  string get_key() override {
     if (position == pair{0, 0})
       return "1";
     else if (position == pair{1, 0})
@@ -57,20 +155,29 @@ struct Keypad
   }
 };
 
-int main()
-{
-  std::ifstream file("../../data/day2_data.txt");
-  std::string line;
-  std::vector<std::string> instructions;
-
-  Keypad keypad = Keypad();
-
-  while (std::getline(file, line))
-  {
-    for (char c : line)
-    {
+void get_code(IKeyboard &keypad, const vector<string> &instructions) {
+  for (const string &instruction: instructions) {
+    for (char c: instruction) {
       keypad.move(c);
     }
-    cout << keypad.get_number();
+    cout << keypad.get_key();
   }
+  cout << endl;
+}
+
+int main() {
+  ifstream file("../../data/day2_data.txt");
+  string line;
+  vector<std::string> instructions;
+  while (getline(file, line)) {
+    instructions.push_back(line);
+  }
+
+  SimpleKeypad keypad = SimpleKeypad();
+
+  get_code(keypad, instructions);
+
+  ComplexKeyboard complex_keyboard = ComplexKeyboard();
+
+  get_code(complex_keyboard, instructions);
 }
