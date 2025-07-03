c27fd77515d9feaf74b64b5dab1b675851e50323
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2025-07-03 21:15:31 +0200

    2016 day15 refactor and docs

--- year2016/cpp/day15/day15.cpp
+++ year2016/cpp/day15/day15.cpp
@@ -11,27 +11,51 @@ using std::sscanf;
 using std::size_t;
 
 
-map<int, int> parse_data() {
+vector<pair<int, int> > parse_data() {
     const auto data = read_lines("../../data/day15_data.txt");
-    map<int, int> disc_positions;
+    vector<pair<int, int> > disc_positions;
 
     for (int i = 0; i < data.size(); i++) {
         const string &line = data[i];
         int positions, start;
         sscanf(line.c_str(), "Disc #%*d has %d positions; at time=0, it is at position %d.", &positions, &start);
 
-        disc_positions.insert({positions, mod(-start - (i + 1), positions)});
+        disc_positions.emplace_back(positions, mod(-start - (i + 1), positions));
     }
 
     return disc_positions;
 }
 
-int find_earliest_time(const map<int, int> &disc_positions) {
-    const auto last = std::prev(disc_positions.end());
-    int inc = last->first;
-    int earliest = last->second;
+/**
+ * For each disc, we want to find a time t such that:
+ *   (position + t + index + 1) ≡ 0 (mod positions)
+ *
+ * This is a congruence. Using modular arithmetic:
+ *   If a ≡ b (mod m), then a - c ≡ b - c (mod m)
+ *
+ * Therefore, we can subtract (position + index + 1) from both sides:
+ *   t ≡ - (position + index + 1) (mod positions)
+ *
+ * Each disc contributes one such congruence. Together, they form a system of
+ * simultaneous congruences:
+ *
+ *   t ≡ a₁ (mod n₁)
+ *   t ≡ a₂ (mod n₂)
+ *   ...
+ *
+ * This system can be solved using the Chinese Remainder Theorem,
+ * provided that all 'positions' values (the moduli nᵢ) are pairwise coprime.
+ */
+int find_earliest_time(vector<pair<int, int> > disc_positions) {
+    std::ranges::sort(disc_positions, [](const auto &a, const auto &b) {
+        return a.first > b.first;
+    });
+    const auto [disc, position] = disc_positions.front();
+    int inc = disc;
+    int earliest = position;
 
-    for (auto [disc, position]: disc_positions) {
+    for (size_t i = 1; i < disc_positions.size(); ++i) {
+        const auto [disc, position] = disc_positions[i];
         while (true) {
             const int earliest_mod_disc = mod(earliest, disc);
             if (earliest_mod_disc == position) {
@@ -41,6 +65,7 @@ int find_earliest_time(const map<int, int> &disc_positions) {
         };
         inc *= disc;
     }
+
     return earliest;
 }
 
@@ -48,24 +73,26 @@ int find_earliest_time(const map<int, int> &disc_positions) {
 // Disc #2 has 2 positions; at time=0, it is at position 1.
 
 int main() {
-    map<int, int> disc_positions{
+    vector<pair<int, int> > disc_positions{
         {5, mod(-4 - 1, 5)},
         {2, mod(-1 - 2, 2)},
     };
-    for (auto [disc, position]: disc_positions) {
-        cout << disc << " " << position << endl;
-    }
 
     int earliest = find_earliest_time(disc_positions);
+    if (earliest != 5) {
+        std::cerr << "Test failed, got: " << earliest << std::endl;
+        return 1;
+    }
     cout << "Test: " << earliest << endl;
 
     disc_positions = parse_data();
     earliest = find_earliest_time(disc_positions);
     cout << "Part 1: " << earliest << endl;
 
-    disc_positions.insert({11, mod(-(static_cast<int>(disc_positions.size() + 1)), 11)});
+    disc_positions.emplace_back(11, mod(-(static_cast<int>(disc_positions.size() + 1)), 11));
     earliest = find_earliest_time(disc_positions);
     cout << "Part 2: " << earliest << endl;
 
     return 0;
 }
+
