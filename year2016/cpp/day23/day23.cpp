cdc10d60ca7eabfb53db1ace42c81ee5a458282a
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2025-07-23 19:56:39 +0200

    2016 day23

--- year2016/cpp/day23/day23.cpp
+++ year2016/cpp/day23/day23.cpp
@@ -1,133 +1,32 @@
 #include <iostream>
-#include <map>
-#include <sstream>
-
-#include "../common/common.h"
 
 using std::cout;
-using std::map;
-using std::string;
-using std::istringstream;
 using std::endl;
-using std::invalid_argument;
-
-// This is one of the top floors of the nicest tower in EBHQ. The Easter Bunny's private office is here, complete with a safe hidden behind a painting, and who wouldn't hide a star in a safe behind a painting?
-//
-// The safe has a digital screen and keypad for code entry. A sticky note attached to the safe has a password hint on it: "eggs". The painting is of a large rabbit coloring some eggs. You see 7.
-//
-// When you go to type the code, though, nothing appears on the display; instead, the keypad comes apart in your hands, apparently having been smashed. Behind it is some kind of socket - one that matches a connector in your prototype computer! You pull apart the smashed keypad and extract the logic circuit, plug it into your computer, and plug your computer into the safe.
-//
-// Now, you just need to figure out what output the keypad would have sent to the safe. You extract the assembunny code from the logic chip (your puzzle input).
-// The code looks like it uses almost the same architecture and instruction set that the monorail computer used! You should be able to use the same assembunny interpreter for this as you did there, but with one new instruction:
-//
-// tgl x toggles the instruction x away (pointing at instructions like jnz does: positive means forward; negative means backward):
-//
-// For one-argument instructions, inc becomes dec, and all other one-argument instructions become inc.
-// For two-argument instructions, jnz becomes cpy, and all other two-instructions become jnz.
-// The arguments of a toggled instruction are not affected.
-// If an attempt is made to toggle an instruction outside the program, nothing happens.
-// If toggling produces an invalid instruction (like cpy 1 2) and an attempt is later made to execute that instruction, skip it instead.
-// If tgl toggles itself (for example, if a is 0, tgl a would target itself and become inc a), the resulting instruction is not executed until the next time it is reached.
-// For example, given this program:
-//
-// cpy 2 a
-// tgl a
-// tgl a
-// tgl a
-// cpy 1 a
-// dec a
-// dec a
-// cpy 2 a initializes register a to 2.
-// The first tgl a toggles an instruction a (2) away from it, which changes the third tgl a into inc a.
-// The second tgl a also modifies an instruction 2 away from it, which changes the cpy 1 a into jnz 1 a.
-// The fourth line, which is now inc a, increments a to 3.
-// Finally, the fifth line, which is now jnz 1 a, jumps a (3) instructions ahead, skipping the dec a instructions.
-// In this example, the final value in register a is 3.
-//
-// The rest of the electronics seem to place the keypad entry (the number of eggs, 7) in register a, run the code, and then send the value left in register a to the safe.
-//
-// What value should be sent to the safe?
-
-int get_value(map<string, int> &registers, const string &value) {
-    int v;
-    if (istringstream iss(value); iss >> v && iss.eof()) {
-        return v;
-    }
-
-    return registers[value];
-}
-
-int run(map<string, int> registers, vector<string> data) {
-    size_t index = 0;
-    while (index < data.size()) {
-        istringstream iss(data[index]);
-        string action, x, y;
-        iss >> action >> x >> y;
-        if (action == "cpy") {
-            if (y != "a" && y != "b" && y != "c" && y != "d") {
-                continue;
-            }
-            registers[y] = get_value(registers, x);
-        } else if (action == "inc") {
-            registers[x]++;
-        } else if (action == "dec") {
-            registers[x]--;
-        } else if (action == "jnz") {
-            if (get_value(registers, x) != 0) {
-                index += get_value(registers, y);
-                continue;
-            }
-        } else if (action == "tgl") {
-            const int i_t = get_value(registers, x);
-            int i = i_t + static_cast<int>(index);
-            if (i < 0 || i >= data.size()) {
-                continue;
-            }
-            istringstream iss_t(data[i]);
-            string action_t, x_t, y_t;
-            iss_t >> action_t >> x_t >> y_t;
-            if (action_t == "inc") {
-                data[i] = "dec " + x_t;
-            } else if (y_t.empty()) {
-                data[i] = "inc " + x_t;
-            } else if (action_t == "jnz") {
-                data[i] = string("cpy ").append(x_t).append(" ").append(y_t);
-            } else {
-                data[i] = string("jnz ").append(x_t).append(" ").append(y_t);
-            }
-        } else {
-            throw invalid_argument("Invalid action: " + action);
-        }
-        index++;
-    }
-
-    return registers["a"];
-}
 
 /*
  * The first instructions down to tgl consist of four loops.
  *
- * The first two increase a.
- * d holds the state of a.
+ * The first two loops increase a.
+ * d holds the current state of a.
  *
  * The first jnz increases by a - x, where x is the loop count.
- * The second jnz repeats the above d times, which ends up giving, e.g., in the first round:
+ * The second jnz repeats the above process d times, which ends up giving, for example, in the first round:
  * a * (a - 1)
  *
- * The third loop changes the tgl pointer, such that
- * every second instruction after tgl is modified.
+* The third loop adjusts the tgl pointer so that,
+ * by the end, every second instruction after tgl is modified.
  *
- * One then jumps back before the first loop, controlled by b,
- * which is decreased for every times the fourth loop is executed.
- * Since b is assigned a - 1 as the second instruction, the three loops end up giving
+ * Execution then jumps back before the first loop, controlled by b,
+ * which is decreased every time the fourth loop is executed.
+ * Since b is assigned a - 1 in the second instruction, the three loops end up giving
  * the factorial of a:
  * a * (a - 1) * (a - 2) * (a - 3) * ... * 1
  *
- * The fourth loop 'breaks' when b = 1, since then b => c => d
- * and when d-- and c++, then tgl changes jnz 1 c to cpy 1 c.
+ * The fourth loop 'breaks' when b = 1, because at that point b → c → d,
+ * and when d-- and c++ occur, tgl changes "jnz 1 c" into "cpy 1 c".
  *
- * tgl has now modified every second instruction, and two small loops are created.
- * The first increases a by 74 (controlled by d), and the second does this 87 times
+ * At this point, tgl has modified every second instruction, creating two small loops.
+ * The first increases a by 74 (controlled by d), and the second repeats this 87 times
  * (controlled by c).
  *
  * Hence, we end up with:
@@ -142,10 +41,6 @@ unsigned long long short_circuit(const unsigned long long a) {
 }
 
 int main() {
-    const auto data = read_lines("../../data/day23_data.txt");
-    const map<string, int> registers = {{"a", 7}};
-
-    // const int part_1 = run(registers, data);
     const auto part_1 = short_circuit(7);
     cout << "Part 1: " << part_1 << endl;
 
