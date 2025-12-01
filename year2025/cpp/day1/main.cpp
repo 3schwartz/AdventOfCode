cf565258ab1a3aa371298e377a6730383a8800c7
Søren Schwartz <40674593+3schwartz@users.noreply.github.com>
2025-12-01 19:25:38 +0100

    2025 day1 refactor

--- year2025/cpp/day1/main.cpp
+++ year2025/cpp/day1/main.cpp
@@ -9,68 +9,41 @@ int main()
     int zero_count = 0;
     int click_count = 0;
 
-    int loop_count = 0;
-    int loop_start = 50;
     for (const string &line : lines)
     {
         int r = std::stoi(line.substr(1));
-        int start_after = start;
+        int position = start;
 
         switch (line[0])
         {
         case 'L':
-            start_after -= r;
+            position -= r;
             break;
         case 'R':
-            start_after += r;
+            position += r;
             break;
         default:
             throw std::invalid_argument("Invalid direction: " + line);
         }
-        for (int i = 1; i <= r; i++)
+
+        if (position <= 0 || 99 < position)
         {
-            if (line[0] == 'L')
-            {
-                loop_start = (loop_start - 1 + 100) % 100;
-            }
-            else
-            {
-                loop_start = (loop_start + 1) % 100;
-            }
-            if (loop_start == 0)
-            {
-                loop_count++;
-            }
+            click_count += std::abs(position) / 100 + (position <= 0 ? 1 : 0);
+        }
+        if (position < 0 && start == 0)
+        {
+            click_count--;
         }
 
-        int start_before = start;
-        start = mod(start_after, 100);
+        start = mod(position, 100);
         if (start == 0)
         {
             zero_count++;
-        }
-        if (start_after > 99)
-        {
-            click_count += start_after / 100;
-        }
-        if (start_after < 0)
-        {
-            click_count += (std::abs(start_after)) / 100 + 1;
-
-            if (start_before == 0)
-            {
-                click_count--;
-            }
-        }
-        if (start_after == 0)
-        {
-            click_count++;
         }
     }
 
     cout << "Part 1: " << zero_count << "\n";
     cout << "Part 2: " << click_count << "\n";
-    cout << "Part 2: " << loop_count << "\n";
 
     return 0;
 }
